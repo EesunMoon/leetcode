960da012129ffb855416460f13f903a170a37677
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-10-19 22:25:18 -0400

    Time: 0 ms (100%), Space: 11.8 MB (28.73%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        
+        map_char = {}
+        map_word = {}
+        
+        words = s.split()
+        
+        # base case
+        if len(pattern) != len(words):
+            return False
+        
+        for char, word in zip(pattern, words):
+            if char not in map_char:
+                if word in map_word:
+                    return False
+                else:
+                    map_char[char] = word
+                    map_word[word] = char
+            else:
+                if map_char[char] != word:
+                    return False
+        
+        return True
+        
