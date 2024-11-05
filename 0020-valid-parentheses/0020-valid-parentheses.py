c48e7d5a02262d951c139207ef40f9a0723928b7
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-11-05 01:49:13 -0500

    Time: 0 ms (100%), Space: 11.6 MB (72.78%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,7 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        
+        stack = []
+        for char in s:
+            if char in "({[":
+                stack.append(char)
+            else:
+                if not stack or (char == ")" and stack[-1] !="(") or (char == "}" and stack[-1]!="{") or (char == "]" and stack[-1]!="["):
+                    return False
+                stack.pop()
+        
+        return not stack
 
+
+
+
+
+        """
         # Solution 2
 
         stack = []
@@ -19,7 +35,7 @@ class Solution(object):
                 stack.pop()
         
         return not stack
-
+        """
         """
         # Solution 1
 
