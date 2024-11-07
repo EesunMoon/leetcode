f7042b3a44ea2555781cdf8eabd8fbcb9514b7b2
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-11-07 17:00:49 -0500

    Using DFS with recursion

---
+++ Concept/Graph/1971_Find_if_Path_Exists_in_Graph_DFS(recursion).py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def validPath(self, n, edges, source, destination):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type source: int
+        :type destination: int
+        :rtype: bool
+        """
+
+        # initialization: store all edges in graph
+        graph = defaultdict(list)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+        
+        seen = [False] * n
+
+        # define DFS function
+        def dfs(curr_node):
+            if curr_node == destination:
+                return True
+            if not seen[curr_node]:
+                seen[curr_node] = True
+                for next_node in graph[curr_node]:
+                    if dfs(next_node):
+                        return True
+            return False
+
+        return dfs(source)
