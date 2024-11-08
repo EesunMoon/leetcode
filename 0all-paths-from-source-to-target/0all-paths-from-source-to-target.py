4a6d2de1087a20bc976cb9d1d9f2b8fb2ad6da3a
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-11-08 15:12:30 -0500

    Time: 7 ms, Memory: 14.8 MB - LeetHub

--- 0all-paths-from-source-to-target/0all-paths-from-source-to-target.py
+++ 0all-paths-from-source-to-target/0all-paths-from-source-to-target.py
@@ -8,20 +8,19 @@ class Solution(object):
         if not graph or len(graph) == 0:
             return path
 
-        n = len(graph)
-        queue = deque()
-        queue.append([0])
-        dest = n-1
-        
-        while queue:
-            curr_path = queue.popleft()
-            curr_node = curr_path[-1]
-            for next_node in graph[curr_node]:
-                temp_path = curr_path[:]
-                temp_path.append(next_node)
-                if next_node == dest:
-                    path.append(temp_path)
-                else:
-                    queue.append(temp_path)
-        return path
+        def dfs(node):
+            curr_path.append(node)
+            if node == dest:
+                paths.append(curr_path[:])
+                return
+            
+            for next_node in graph[node]:
+                dfs(next_node)
+                curr_path.pop()
+
+        dest = len(graph)-1
+        paths = []
+        curr_path = []
+        dfs(0)
+        return paths
         
