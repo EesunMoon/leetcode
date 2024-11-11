adb7e9b91b3134157706f297697698980c3d7309
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-11-10 19:01:20 -0500

    Time: 183 ms (98.59%), Space: 26.9 MB (72.69%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,36 +1,3 @@
-class UnionFind:
-    def __init__(self, grid):
-        self.count = 0
-        m,n = len(grid), len(grid[0])
-        self.root = []
-        self.rank = []
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == '1':
-                    self.root.append(i*n+j)
-                    self.count +=1
-                else:
-                    self.root.append(-1)
-                self.rank.append(0)
-    def find(self, i):
-        if self.root[i]!=i:
-            self.root[i] = self.find(self.root[i])
-        return self.root[i]
-    def union(self, x, y):
-        rootx = self.find(x)
-        rooty = self.find(y)
-        if rootx != rooty:
-            if self.rank[rootx] > self.rank[rooty]:
-                self.root[rooty] = rootx
-            elif self.rank[rootx] < self.rank[rooty]:
-                self.root[rootx] = rooty
-            else:
-                self.root[rootx] = rooty
-                self.rank[rooty] += 1
-            self.count -=1
-    def getCount(self):
-        return self.count
-
 class Solution(object):
     def numIslands(self, grid):
         """
@@ -40,19 +7,29 @@ class Solution(object):
         if not grid or not grid[0]:
             return 0
         
-        nr, nc = len(grid), len(grid[0])
-        uf = UnionFind(grid)
+        num_islands = 0
+        self.m, self.n = len(grid), len(grid[0])
 
-        for r in range(nr):
-            for c in range(nc):
-                if grid[r][c] == "1":
-                    grid[r][c] == "0" # Flag to find
-                    if r-1 >=0 and grid[r-1][c] == "1":
-                        uf.union(r*nc+c, (r-1)*nc+c)
-                    if r+1 <nr and grid[r+1][c]=="1":
-                        uf.union(r*nc+c, (r+1)*nc+c)
-                    if c-1 >=0 and grid[r][c-1] == "1":
-                        uf.union(r*nc+c, r*nc+c-1)
-                    if c+1 < nc and grid[r][c+1]=="1":
-                        uf.union(r*nc+c, r*nc+c+1)
-        return uf.getCount()
+        for i in range(self.m):
+            for j in range(self.n):
+                if grid[i][j] == "1":
+                    grid[i][j] = "0" # mark as visited
+                    num_islands += 1
+                    queue = deque()
+                    queue.append((i,j))
+                    while queue:
+                        row, col = queue.popleft()
+                        if row-1 >=0 and grid[row-1][col] == "1":
+                            queue.append((row-1, col))
+                            grid[row-1][col] = "0"
+                        if row+1 < self.m and grid[row+1][col] == "1":
+                            queue.append((row+1, col))
+                            grid[row+1][col]="0"
+                        if col-1 >=0 and grid[row][col-1] == "1":
+                            queue.append((row, col-1))
+                            grid[row][col-1] ="0"
+                        if col+1 < self.n and grid[row][col+1] == "1":
+                            queue.append((row, col+1))
+                            grid[row][col+1]="0"
+
+        return num_islands
