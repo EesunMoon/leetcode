97724fd1f712f9c49e6ba2be351d371bb4b7518f
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-11-12 17:12:04 -0500

    Time: 173 ms (64.41%), Space: 21.4 MB (80.27%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maximumGap(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 2:
+            return 0
+
+        nums.sort()
+        max_diff = 0
+        for i in range(len(nums)-1):
+            max_diff = max(max_diff, nums[i+1] - nums[i])
+        
+        return max_diff
