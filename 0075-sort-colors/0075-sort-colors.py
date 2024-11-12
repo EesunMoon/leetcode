417d1df5f0017f9509d20fefb2627962a64563b7
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-11-12 02:19:59 -0500

    Time: 0 ms (100%), Space: 11.8 MB (7.59%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -5,18 +5,20 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         
-        left, curr = 0, 0
-        right = len(nums)-1
-
-        while curr <= right:
-            if nums[curr] == 0:
-                nums[curr], nums[left] = nums[left], nums[curr]
-                left += 1
-                curr += 1
-            elif nums[curr] == 2:
-                nums[curr], nums[right] = nums[right], nums[curr]
-                right -= 1
-            else:
-                curr += 1
-            
+        # counting sort
+        counts = [0] * (2+1)
+        for num in nums:
+            counts[num] += 1
+        start = 0
+        for i, cnt in enumerate(counts):
+            counts[i] = start
+            start += cnt
+        
+        sorted_nums = [0] * len(nums)
+        for num in nums:
+            sorted_nums[counts[num]] = num
+            counts[num] += 1
+        
+        for i in range(len(nums)):
+            nums[i] = sorted_nums[i]
         return nums
