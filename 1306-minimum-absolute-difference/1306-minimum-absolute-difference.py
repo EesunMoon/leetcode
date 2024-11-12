73fd48077ee6c72bd8766f8f520e063549c4ae75
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-11-12 15:50:03 -0500

    Time: 47 ms (98.44%), Space: 20.9 MB (71.68%) - LeetHub

--- 1306-minimum-absolute-difference/1306-minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/1306-minimum-absolute-difference.py
@@ -4,24 +4,17 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[List[int]]
         """
-        min_element = min(arr)
-        max_element = max(arr)
-        count = [0] * (max_element - min_element + 1)
 
-        for ele in arr:
-            count[ele-min_element] += 1
-        
-        prev = 0
-        min_diff = max_element - min_element
+        arr.sort()
+        min_diff = float("inf")
+
         answer = []
-        for curr in range(1, len(count)):
-            if count[curr] == 0:
-                continue
-            if curr- prev == min_diff:
-                answer.append([prev+min_element, curr+min_element])
-            elif curr-prev < min_diff:
-                min_diff = curr- prev
-                answer = [[prev+min_element, curr+min_element]]
+        for i in range(len(arr)-1):
+            curr_diff = arr[i+1]- arr[i]
+            if curr_diff == min_diff:
+                answer.append([arr[i], arr[i+1]])
+            elif curr_diff < min_diff:
+                min_diff = curr_diff
+                answer = [[arr[i], arr[i+1]]]
         
-            prev = curr
         return answer
