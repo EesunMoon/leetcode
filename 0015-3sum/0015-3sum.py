296b65c283d2219d09feda5bf9253de903a7e7a7
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-11-25 15:27:10 -0500

    Time: 715 ms (74.28%), Space: 17.6 MB (22.79%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,27 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        # sort
+        nums.sort() # O(nlogn)
         result = []
 
-        nums.sort() # O(nlogn)
-
-        # the solution set must not contain duplicate triplets
-
+        # the solution set must no contain duplicate triplets
         for k in range(len(nums)):
-            if k >0 and nums[k] == nums[k-1]:
+            # fixed nums[k]
+            if k !=0 and nums[k]==nums[k-1]:
                 continue
             
             i, j = k+1, len(nums)-1
             while i<j:
-                candidate = nums[k] + nums[i] + nums[j]
-
-                if candidate < 0:
-                    i += 1
-                elif candidate >0:
+                triple = nums[k] + nums[i] + nums[j]
+                if triple > 0:
                     j-=1
+                elif triple < 0:
+                    i+=1
                 else:
                     result.append([nums[k], nums[i], nums[j]])
                     i+=1
                     while i<j and nums[i] == nums[i-1]:
                         i+=1
+                    
         return result
