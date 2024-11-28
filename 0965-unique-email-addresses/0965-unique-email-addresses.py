759c5388c48f4cbde83e92935099370369b09833
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-11-28 13:51:18 -0500

    Time: 6 ms (78.03%), Space: 12.3 MB (17.73%) - LeetHub

--- 0965-unique-email-addresses/0965-unique-email-addresses.py
+++ 0965-unique-email-addresses/0965-unique-email-addresses.py
@@ -4,24 +4,16 @@ class Solution(object):
         :type emails: List[str]
         :rtype: int
         """
-        unique = defaultdict(list) # domain: local
-        cnt = 0
+        unique = set()
         
         for email in emails:
             # 1) seperate @ => local / domain
-            idx = email.index("@")
-            local = email[:idx]
-            domain = email[idx+1:]
+            local, domain = email.split("@")
 
             # 2) apply rule in local address
-            local = local.replace('.', "")
-            if "+" in local:
-                plus = local.index("+")
-                local = local[:plus]
+            local = local.split("+")[0].replace(".", "")
 
             # 3) check
-            if local not in unique[domain]:
-                unique[domain].append(local)
-                cnt += 1
+            unique.add(local + "@" + domain)
 
-        return cnt
+        return len(unique)
