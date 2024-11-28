f6524dcf45908f85ae59dd6b00c7ac0e9a40e02e
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-11-27 23:06:05 -0500

    Time: 2 ms (5.48%), Space: 12.3 MB (8.06%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -14,11 +14,8 @@ class Solution(object):
         """
         if not p and not q:
             return True
-
-        if not p or not q:
-            return False
         
-        if p.val == q.val:
+        if p and q and p.val == q.val:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         else:
             return False
