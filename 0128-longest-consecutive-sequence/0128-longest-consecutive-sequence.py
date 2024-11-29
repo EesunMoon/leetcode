0aa394643a738f757bae788ca5dca3a862136d5b
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-11-28 19:58:35 -0500

    Time: 4766 ms (11.21%), Space: 26.6 MB (42.34%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -4,17 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # if) sorting => O(nlogn)
-        # solve without sorting
 
         longest = 0
         setnums = set(nums)
 
-        for num in setnums:
-            if (num-1) not in setnums: # starting point
+        for num in nums:
+            if num-1 not in setnums:
                 length = 1
-                while (num + length) in setnums:
+                while num+length in setnums:
                     length += 1
+                
                 longest = max(longest, length)
-
         return longest
+
+       
