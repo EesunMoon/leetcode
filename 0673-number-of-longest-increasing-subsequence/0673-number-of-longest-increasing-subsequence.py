a9c42e75144d6885a8afaeb895e9d7826db947d5
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-11-30 13:36:10 -0500

    Time: 857 ms (37.27%), Space: 12.3 MB (8.13%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
@@ -10,17 +10,18 @@ class Solution(object):
 
         for i in range(n):
             for j in range(i):
-                if nums[i] > nums[j]:
-                    if length[j] + 1 > length[i]:
+                if nums[j] < nums[i]:
+                    if length[i] < length[j]+1:
                         length[i] = length[j]+1
                         count[i] = 0
-                    if length[j] + 1 == length[i]:
+                    if length[i] == length[j]+1:
                         count[i] += count[j]
-
+        
         max_length = max(length)
         result = 0
-
+    
         for i in range(n):
-            if length[i] == max_length:
+            if length[i]==max_length:
                 result += count[i]
+
         return result
