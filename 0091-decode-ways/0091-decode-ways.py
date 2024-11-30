7281a561b0a00d6b088746daf4b5d16cd942eac9
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-11-30 13:50:37 -0500

    Time: 3 ms (48.43%), Space: 12.1 MB (15.73%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -4,22 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        dp = {len(s):1}
 
-        dp = {len(s):1} # base case
-
-        n = len(s)
-
-        # bottom up approach
-        for i in range(n-1, -1, -1):
+        for i in range(len(s)-1, -1, -1):
             if s[i] == "0":
                 dp[i] = 0
             else:
                 dp[i] = dp[i+1]
             
-            # 2-digit case
-            if (i+1 < n):
-                if (s[i] =="1" ) or (s[i]=="2" and s[i+1] in "0123456"):
-                    dp[i] += dp[i+2]
+            # 2- digit
+            if (i+1 < len(s)) and (s[i] == "1" or s[i]=="2" and s[i+1] in "0123456"):
+                dp[i] += dp[i+2]
         
         return dp[0]
 
