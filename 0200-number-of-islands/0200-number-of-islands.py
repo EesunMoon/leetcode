6f018506e71a335f89968bffe50ab77c08b90b6c
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-12-01 16:08:52 -0500

    Time: 219 ms (60.52%), Space: 27.4 MB (46.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,32 +4,29 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        if not grid or not grid[0]:
-            return 0
+        ROWS, COLS = len(grid), len(grid[0])
+        cnt = 0
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+        def bfs(r, c):
+            queue = deque()
+            queue.append((r, c))
+            grid[r][c] = "0" # visited mark
+
+            while queue:
+                r, c = queue.popleft()
+                for dr, dc in directions:
+                    candr, candc = dr + r, dc + c
+                    if (candr not in (-1, ROWS) and candc not in (-1, COLS)):
+                        if grid[candr][candc]== "1":
+                            queue.append((candr, candc))
+                            grid[candr][candc] = "0" # visited mark
+
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    bfs(r, c)
+                    cnt += 1
         
-        num_islands = 0
-        self.m, self.n = len(grid), len(grid[0])
-
-        for i in range(self.m):
-            for j in range(self.n):
-                if grid[i][j] == "1":
-                    grid[i][j] = "0" # mark as visited
-                    num_islands += 1
-                    queue = deque()
-                    queue.append((i,j))
-                    while queue:
-                        row, col = queue.popleft()
-                        if row-1 >=0 and grid[row-1][col] == "1":
-                            queue.append((row-1, col))
-                            grid[row-1][col] = "0"
-                        if row+1 < self.m and grid[row+1][col] == "1":
-                            queue.append((row+1, col))
-                            grid[row+1][col]="0"
-                        if col-1 >=0 and grid[row][col-1] == "1":
-                            queue.append((row, col-1))
-                            grid[row][col-1] ="0"
-                        if col+1 < self.n and grid[row][col+1] == "1":
-                            queue.append((row, col+1))
-                            grid[row][col+1]="0"
-
-        return num_islands
+        return cnt
