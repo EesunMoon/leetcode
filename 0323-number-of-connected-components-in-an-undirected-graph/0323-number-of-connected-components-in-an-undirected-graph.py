8668a9a93eff326267c88ed7381af3cee43cebb5
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-12-03 14:41:06 -0500

    Time: 7 ms (85.1%), Space: 12.9 MB (67.5%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -5,26 +5,36 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: int
         """
-        adj = [[] for _ in range(n)]
+        par = [i for i in range(n)]
+        rank = [1]*n
 
-        for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
-
-        visited = set()
-        def dfs(curr):
-            if curr in visited:
-                return
-
-            visited.add(curr)
-            for nei in adj[curr]:
-                dfs(nei)
-
-        res = 0
-        for i in range(n):
-            if i not in visited:
-                dfs(i)
-                res += 1
-                
-        return res
+        def find(node):
+            p = par[node]
+            
+            while p != par[p]:
+                par[p] = par[par[p]]
+                p = par[p]
+            
+            return p
         
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return False
+            else:
+                if rank[p1] > rank[p2]:
+                    par[p2] = p1
+                    rank[p1] += rank[p2]
+                else:
+                    par[p1] = p2
+                    rank[p2] += rank[p1]
+            
+            return True
+        
+        res = n
+        for u, v in edges:
+            if union(u, v):
+                res -= 1
+        
+        return res
