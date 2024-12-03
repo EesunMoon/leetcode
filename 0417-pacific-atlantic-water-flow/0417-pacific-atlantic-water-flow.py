24f27eeeefe8b1a79bd8195dadb9877aca69f318
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-12-03 14:26:28 -0500

    Time: 48 ms (46.47%), Space: 13.8 MB (12.17%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -5,18 +5,21 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         ROWS, COLS = len(heights), len(heights[0])
-        pac, atl = set(), set()
+        pac, atl = set(), set() # marked as visited
 
-        def dfs(r, c, seen, prevHeight):
-            if r in (-1, ROWS) or c in (-1, COLS) or (r, c) in seen or heights[r][c] < prevHeight:
+        def dfs(r, c, seen, preHeights):
+            # base case
+            if r in (-1, ROWS) or c in (-1, COLS) or (r, c) in seen or heights[r][c] < preHeights:
                 return
-
+            
             seen.add((r, c))
-            dfs(r+1, c, seen, heights[r][c])
+            
             dfs(r-1, c, seen, heights[r][c])
-            dfs(r, c+1, seen, heights[r][c])
+            dfs(r+1, c, seen, heights[r][c])
             dfs(r, c-1, seen, heights[r][c])
-
+            dfs(r, c+1, seen, heights[r][c])
+            
+        # search
         for r in range(ROWS):
             dfs(r, 0, pac, heights[r][0])
             dfs(r, COLS-1, atl, heights[r][COLS-1])
@@ -24,7 +27,8 @@ class Solution(object):
         for c in range(COLS):
             dfs(0, c, pac, heights[0][c])
             dfs(ROWS-1, c, atl, heights[ROWS-1][c])
-        
+
+        # result
         res = []
         for r in range(ROWS):
             for c in range(COLS):
@@ -32,5 +36,3 @@ class Solution(object):
                     res.append([r, c])
 
         return res
-                
-
