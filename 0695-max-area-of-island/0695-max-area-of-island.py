ed5d5b9806b7c7dedac9e3bdb9af686be274852e
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-12-02 19:47:45 -0500

    Time: 36 ms (48.2%), Space: 14.7 MB (31.68%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -4,20 +4,22 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        ROWS, COLS = len(grid), len(grid[0])
         maxArea = 0
-        visited = set()
+        ROWS, COLS = len(grid),len(grid[0])
+        # visited = set()
 
         def dfs(r, c):
-            if r in (-1, ROWS) or c in (-1, COLS) or grid[r][c] == 0 or (r, c) in visited:
+            if r in (-1, ROWS) or c in (-1, COLS) or grid[r][c] == 0:
                 return 0
-            
-            visited.add((r, c))
-            return (1 + dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1))
+            # visited.add((r, c))
+            grid[r][c] = 0
+            return 1 + dfs(r-1, c) + dfs(r+1, c) + dfs(r, c+1) + dfs(r, c-1)
 
         for r in range(ROWS):
             for c in range(COLS):
-                maxArea = max(dfs(r, c), maxArea)
-
+                if grid[r][c] == 1:
+                    maxArea = max(dfs(r, c), maxArea)
+        
         return maxArea
+                
         
