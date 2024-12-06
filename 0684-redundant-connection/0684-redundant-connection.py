bc76416d8d9a7991bb77f3c86ec910126317a40f
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-12-06 14:08:35 -0500

    Time: 3 ms (73.37%), Space: 12.6 MB (25.46%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -4,28 +4,31 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: List[int]
         """
-        
-        adj = [[] for i in range(len(edges)+1)]
-        # visited = set()
+        par = [i for i in range(len(edges)+1)]
+        rank = [1] * (len(edges)+1)
 
-        def dfs(curr, prev):
-            if curr in visited:
+        def find(n):
+            p = par[n]
+            while p != par[p]:
+                par[p] = par[par[p]]
+                p = par[p]
+            return p
+        
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
                 return False
-            
-            visited.add(curr)
-            for nei in adj[curr]:
-                if nei == prev:
-                    continue
-                
-                if not dfs(nei, curr):
-                    return False
-            
+            else:
+                if rank[p1] > rank[p2]:
+                    par[p2]=p1
+                    rank[p1] += rank[p2]
+                else:
+                    par[p1]=p2
+                    rank[p2]+=rank[p1]
             return True
         
         for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
-            visited = set()
-
-            if not dfs(u, -1):
+            if not union(u, v):
                 return [u, v]
+        
