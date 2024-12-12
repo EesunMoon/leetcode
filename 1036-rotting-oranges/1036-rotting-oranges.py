ab3b4aac1790f6ae28d4faa0ec65bfcca4597d34
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-12-11 22:08:58 -0500

    Time: 6 ms (60.6%), Space: 12.1 MB (16.09%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -4,40 +4,35 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        # initialization
         fresh = 0
-        queue = deque()
         ROWS, COLS = len(grid), len(grid[0])
-        directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
-        res = 1
+        queue = deque()
 
-        # initialize
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
                     fresh += 1
                 elif grid[r][c] == 2:
-                    queue.append((r, c))
-                    grid[r][c] = 0 # mark as visited
+                    queue.append((r,c))
         
+        # edge case
         if fresh == 0:
             return 0
-        
-        # track
-        while queue:
-            # level
+
+        # bfs
+        res = 0
+        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        while queue and fresh > 0:
             for _ in range(len(queue)):
                 r, c = queue.popleft()
                 for dr, dc in directions:
-                    candr, candc = dr+r, dc+c
-                    if (candr in (-1, ROWS) or candc in (-1, COLS) 
-                        or grid[candr][candc] == 0):
+                    candr, candc = dr + r, dc + c
+                    if candr in (-1, ROWS) or candc in (-1, COLS) or grid[candr][candc] != 1:
                         continue
                     queue.append((candr, candc))
-                    grid[candr][candc] = 0 # mark as visited
                     fresh -= 1
-            # print(queue, res)
-            if fresh == 0:
-                break
-            res += 1                        
+                    grid[candr][candc] = 2
+            res += 1
 
         return res if fresh == 0 else -1
