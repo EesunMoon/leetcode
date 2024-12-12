c9412ae9ad9a33e0d6b6a67745d07d9ac14a2b1d
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-12-12 18:16:11 -0500

    Time: 9 ms (65.19%), Space: 14.1 MB (44.16%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,24 +5,21 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        # hashmap => key: freq O(n) n: #.of nums
+        hashmap = {}
+        for n in nums:
+            hashmap[n] = 1 + hashmap.get(n, 0)
         
-        # 1) hashmap => O(n)
-        count = {}
-        for num in nums:
-            count[num] = 1 + count.get(num,0)
+        # top k freq elements O(nlogk)
+        q = []
+        for n, freq in hashmap.items():
+            heapq.heappush(q, [freq, n])
+            if len(q) > k:
+                heapq.heappop(q)
         
-        # 2) heapify
-        heap = []
-        for nums, freq in count.items():
-            heapq.heappush(heap, (freq, nums))
-
-            if len(heap) > k:
-                heapq.heappop(heap)
-        
-        # 3) result
-        res = []
-        for i in range(k):
-            freq, ele = heapq.heappop(heap)
-            res.append(ele)
-        
-        return res
+        # make answer O(klogk)
+        answer = []
+        for _ in range(k):
+            freq, n = heapq.heappop(q)
+            answer.append(n)
+        return answer
