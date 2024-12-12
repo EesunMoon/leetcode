9569b23f787a1741bed72ead4b2614b77700f16d
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-12-12 00:43:57 -0500

    Time: 292 ms (53.21%), Space: 78.5 MB (5.19%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,24 +1,27 @@
 class Node():
-    def __init__(self, key, value):
-        self.key, self.value = key, value
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
         self.prev, self.next = None, None
 
 class LRUCache(object):
+
     def __init__(self, capacity):
         """
         :type capacity: int
         """
-        self.size = capacity
-        self.cache = {}
-        self.left, self.right = Node(0,0), Node(0,0)
+        self.capacity = capacity
+        self.hashmap = {} # key: address
+        self.left, self.right = Node(None, None), Node(None, None)
         self.left.next, self.right.prev = self.right, self.left
-
-    def insert(self, node):
-        prv, nxt = self.right.prev, self.right
-        prv.next = nxt.prev = node
-        node.prev, node.next = prv, nxt
-        
-    def delete(self, node):
+    
+    def addNode(self, node):
+        # put in the rightmost
+        prv = self.right.prev
+        prv.next = self.right.prev = node
+        node.prev, node.next = prv, self.right
+    
+    def deleteNode(self, node):
         prv, nxt = node.prev, node.next
         prv.next, nxt.prev = nxt, prv
 
@@ -27,13 +30,15 @@ class LRUCache(object):
         :type key: int
         :rtype: int
         """
-        if key not in self.cache:
+        # if the key exist, or not
+        if key not in self.hashmap:
             return -1
         
-        node = self.cache[key]
-        self.delete(node)
-        self.insert(node)
-        return node.value
+        # update cache: delete -> add
+        target = self.hashmap[key]
+        self.deleteNode(target)
+        self.addNode(target)
+        return target.val
 
     def put(self, key, value):
         """
@@ -41,17 +46,22 @@ class LRUCache(object):
         :type value: int
         :rtype: None
         """
-        if key in self.cache:
-            self.delete(self.cache[key])
-        self.cache[key] = Node(key, value) # new node
-        self.insert(self.cache[key])
-
-        if len(self.cache) > self.size:
-            # leftmost cache
+        if key in self.hashmap:
+            target = self.hashmap[key]
+            self.deleteNode(target)
+        
+        new = Node(key, value)
+        self.hashmap[key] = new
+        self.addNode(new)
+        
+        if len(self.hashmap) > self.capacity:
+            # delete the least recently used key
             lru = self.left.next
-            self.delete(lru)
-            del self.cache[lru.key]
-    
+            del self.hashmap[lru.key]
+            self.deleteNode(lru)
+
+        
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
