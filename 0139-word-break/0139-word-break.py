bb862549d2c13014deb9873b96b9fbf3a902d5c4
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-12-12 12:19:33 -0500

    Time: 4 ms (71.52%), Space: 12.3 MB (10%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -5,15 +5,23 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: bool
         """
-        # base case
+        
+        # DSA: DP
+        # idx : #. string
+        # state: possible or not => True, False
+        # recurrence variable: True, False
+
         dp = [False] * (len(s)+1)
         dp[len(s)] = True
-        
+
         for i in range(len(s)-1, -1, -1):
             for w in wordDict:
-                if i+len(w) <= len(s) and s[i:i+len(w)] == w:
+                if i + len(w) <= len(s) and s[i:i+len(w)] == w:
                     dp[i] = dp[i+len(w)]
                 if dp[i]:
                     break
-        return dp[0]
         
+        return dp[0]
+
+        # S O(s) s: length of the string
+        # T O(s*w) w: #.of the wordDict
