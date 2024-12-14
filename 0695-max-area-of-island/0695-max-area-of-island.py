4807f47b1653a4d06fff25c5c4bdcedcdb19f905
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-12-14 14:04:46 -0500

    Time: 52 ms (19.7%), Space: 15.6 MB (8.01%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -11,15 +11,13 @@ class Solution(object):
             if (r in (-1, ROWS) or c in (-1, COLS) or grid[r][c] == 0 or (r, c) in seen):
                 return 0
             
-            if grid[r][c] == 1:
-                seen.add((r, c))
-                return 1 + dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1)
+            seen.add((r, c))
+            return 1 + dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1)
         
         maximum = 0
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == 1:
-                    maximum = max(maximum, dfs(r, c))
+                maximum = max(maximum, dfs(r, c))
 
         return maximum
         
