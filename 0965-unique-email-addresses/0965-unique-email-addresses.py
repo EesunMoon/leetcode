4319c001b395e02d680b25c07aab334315a42c60
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-12-17 15:53:50 -0500

    Time: 5 ms (82.84%), Space: 12.5 MB (16.42%) - LeetHub

--- 0965-unique-email-addresses/0965-unique-email-addresses.py
+++ 0965-unique-email-addresses/0965-unique-email-addresses.py
@@ -5,15 +5,17 @@ class Solution(object):
         :rtype: int
         """
         unique = set()
-        
+
         for email in emails:
-            # 1) seperate @ => local / domain
+            # 1) split the local name, domain name
             local, domain = email.split("@")
 
-            # 2) apply rule in local address
+            # 2) [local name] "." <- remove
+            # 3) [local name] "+":: remove everthing after + sign
             local = local.split("+")[0].replace(".", "")
 
-            # 3) check
-            unique.add(local + "@" + domain)
-
+            # 4) add in the set: local + domain
+            unique.add(local+"@"+domain)
+        
         return len(unique)
+            
