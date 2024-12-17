f5224a138afcc1a151b252c186acbaa49df7724a
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-12-17 16:42:08 -0500

    Time: 0 ms (100%), Space: 12.3 MB (5.86%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -6,15 +6,16 @@ class Solution(object):
         """
         dp = {len(s):1}
 
+        # 11106
         for i in range(len(s)-1, -1, -1):
             if s[i] == "0":
                 dp[i] = 0
             else:
+                # 1-digit case
                 dp[i] = dp[i+1]
             
-            # 2- digit
-            if (i+1 < len(s)) and (s[i] == "1" or s[i]=="2" and s[i+1] in "0123456"):
+            # 1-digit case
+            if ((i+1 < len(s) and (s[i]=="1" or s[i]=="2" and s[i+1] in "0123456"))):
                 dp[i] += dp[i+2]
         
         return dp[0]
-
