7726104d66378a2f1843817c425e7e18b1123fc6
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-12-21 21:10:00 -0500

    Time: 0 ms (100%), Space: 12.6 MB (5.15%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.py
+++ 1127-last-stone-weight/1127-last-stone-weight.py
@@ -1,19 +1,21 @@
 class Solution(object):
     def lastStoneWeight(self, stones):
-        """
-        :type stones: List[int]
-        :rtype: int
-        """
-        max_heap = []
-        for stone in stones:
-            heapq.heappush(max_heap, -stone)
-        
-        while len(max_heap) > 1:
-            y = -heapq.heappop(max_heap)
-            x = -heapq.heappop(max_heap)
-            if x == y:
-                continue
-            else:
-                heapq.heappush(max_heap, x-y)
+        maxHeap = []
 
-        return -max_heap[0] if max_heap else 0
+        if len(stones) == 1:
+            return stones[0]
+
+        for stone in stones:
+            heapq.heappush(maxHeap, -stone)
+        
+        while maxHeap:
+            y = -heapq.heappop(maxHeap)
+            x = -heapq.heappop(maxHeap)
+
+            if x < y:
+                heapq.heappush(maxHeap, -abs(y-x))
+            
+            if len(maxHeap) == 1:
+                return -maxHeap[0]
+        
+        return 0
