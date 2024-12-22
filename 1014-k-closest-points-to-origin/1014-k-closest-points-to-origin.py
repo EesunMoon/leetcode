8298b1da8fb68a13efec8244386cada4c071ba7e
Eesun <55704325+EesunMoon@users.noreply.github.com>
2024-12-21 21:10:32 -0500

    Time: 179 ms (12.16%), Space: 19.1 MB (5.27%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -1,22 +1,18 @@
 class Solution(object):
     def kClosest(self, points, k):
-        """
-        :type points: List[List[int]]
-        :type k: int
-        :rtype: List[List[int]]
-        """
-        def calDist(a, b):
-            return (a-0)**2 + (b-0)**2
-        
-        # first k elements
-        heap = [(-calDist(points[i][0], points[i][1]), i) for i in range(k)]
-        heapq.heapify(heap)
+        dist = []
+        def euclidean(x, y):
+            return x**2 + y**2
 
-        for i in range(k, len(points)):
-            dist = -calDist(points[i][0], points[i][1])
-            
-            if dist > heap[0][0]:
-                heapq.heappushpop(heap, (dist, i)) # delete & add
-                
-            
-        return [points[i] for (_, i) in heap]
+        for x, y in points:
+            heapq.heappush(dist, [-euclidean(x, y), [x, y]])
+
+            if len(dist) > k:
+                heapq.heappop(dist)
+        
+        ans = []
+        while dist:
+            d, point = heapq.heappop(dist)
+            ans.append(point)
+        
+        return ans
