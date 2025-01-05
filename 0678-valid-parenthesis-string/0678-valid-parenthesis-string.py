77ef54023bf5c9fd4e6997c138deaf887f6f46f3
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-05 23:04:22 +0900

    Time: 0 ms (100%), Space: 12.4 MB (13.28%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -5,18 +5,18 @@ class Solution(object):
         :rtype: bool
         """
         leftMin, leftMax = 0, 0
+
         for c in s:
             if c == "(":
                 leftMin, leftMax = leftMin + 1, leftMax + 1
             elif c == ")":
                 leftMin, leftMax = leftMin - 1, leftMax - 1
-            else:
+            elif c == "*":
                 leftMin, leftMax = leftMin - 1, leftMax + 1
             
-            if leftMax < 0:
-                return False
             if leftMin < 0:
                 leftMin = 0
-
+            if leftMax < 0:
+                return False
         return leftMin == 0
         
