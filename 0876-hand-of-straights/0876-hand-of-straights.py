c6d8efeda70489e12ef3206db52004269984b5bb
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-05 23:25:55 +0900

    Time: 112 ms (41.79%), Space: 14.4 MB (22.39%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -5,26 +5,21 @@ class Solution(object):
         :type groupSize: int
         :rtype: bool
         """
-        # base case
-        if len(hand) % groupSize:
-            return False
-        
-        # hashmap
         count = {}
         for num in hand:
             count[num] = 1 + count.get(num, 0)
         
-        for card in hand:
-            # find the valid start card
-            start = card
-            while count.get(start-1,0):
-                start -= 1
-            
-            while start <= card:
-                while count[start]:
-                    for next_card in range(start, start+groupSize):
-                        if not count.get(next_card, 0):
-                            return False
-                        count[next_card] -= 1
-                start += 1
+        minH = list(count.keys())
+        heapq.heapify(minH)
+
+        while minH:
+            start = minH[0]
+            for num in range(start, start+groupSize):
+                if num not in count:
+                    return False
+                count[num] -= 1
+                if count[num] == 0:
+                    if num != minH[0]:
+                        return False
+                    heapq.heappop(minH)
         return True
