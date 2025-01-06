cd3e38c0577f0f88f578e4c4e678ebc9eeafc2c9
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-06 13:30:05 +0900

    Time: 9 ms (26.72%), Space: 12.6 MB (14.81%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -6,22 +6,16 @@ class Solution(object):
         """
         # hashmap
         count = {}
-        for c in s:
-            count[c] = 1 + count.get(c, 0)
+        for i, c in enumerate(s):
+            count[c] = i
         
-        l = 0
         res = []
-        target = set()
-        for r in range(len(s)):
-            count[s[r]] -= 1
-            target.add(s[r])
-            flag = True
-            for c in target:
-                if count[c] != 0:
-                    flag = False
-                    break
-            if flag:
-                res.append(r-l+1)
-                l = r+1
-                target = set()
+        size, end = 0, 0
+        for i, c in enumerate(s):
+            size += 1
+            end = max(end, count[c])
+
+            if i == end:
+                res.append(size)
+                size = 0
         return res
