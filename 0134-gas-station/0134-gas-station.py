4ddcd6b501e18ca1db0ba0b0b354f64c4be6e754
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-06 12:57:25 +0900

    Time: 20 ms (89.74%), Space: 17.9 MB (33.86%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -5,14 +5,20 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
+        # guaranteed to be unique
+        # subSum > 0 :: start
+        # total gas < total cost :: impossible
+
         # base case
         if sum(gas) < sum(cost):
             return -1
         
-        total, idx = 0, 0
+        total = 0
+        res = 0
         for i in range(len(gas)):
-            total += (gas[i]-cost[i])
+            total += (gas[i] - cost[i])
+
             if total < 0:
-                idx = i + 1
+                res = i+1 # starting from next index
                 total = 0
-        return idx
+        return res
