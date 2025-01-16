ade9fedc7be31a794b3f2bda6ab1184badf6ca65
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-16 12:09:29 +0900

    Time: 0 ms (100%), Space: 12.4 MB (27.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,22 +1,29 @@
 class Solution(object):
-    def climb_stairs(self, i, n, cache):
-        if i > n:
-            return 0
-        if i == n:
-            return 1
-        if cache[i] > 0:
-            return cache[i]
-        
-        cache[i] = self.climb_stairs(i+1, n, cache) + self.climb_stairs(i+2, n, cache)
-        
-        return cache[i]
-
     def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        cache = [0] * (n+1)
-        return self.climb_stairs(0, n, cache)
+        one, two= 1, 1 # base case
+        for _ in range(n-1):
+            tmp = one
+            one = one + two
+            two = tmp
+        return one
+    # def climb_stairs(self, i, n, cache):
+    #     if i > n:
+    #         return 0
+    #     if i == n:
+    #         return 1
+    #     if cache[i] > 0:
+    #         return cache[i]
+        
+    #     cache[i] = self.climb_stairs(i+1, n, cache) + self.climb_stairs(i+2, n, cache)
+        
+    #     return cache[i]
+
+    # def climbStairs(self, n):
+    #     """
+    #     :type n: int
+    #     :rtype: int
+    #     """
+    #     cache = [0] * (n+1)
+    #     return self.climb_stairs(0, n, cache)
         
         
