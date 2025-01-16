4ab543c57834126abb8564e71c5853bd98c844f7
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-16 12:38:39 +0900

    Time: 3 ms (73.03%), Space: 12.6 MB (37.93%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,5 +1,11 @@
 class Solution(object):
     def minCostClimbingStairs(self, cost):
+        cost.append(0)
+        for i in range(len(cost)-3, -1, -1):
+            cost[i] += min(cost[i+1], cost[i+2])
+        return min(cost[0], cost[1])
+    '''
+    def minCostClimbingStairs(self, cost):
         N = len(cost)
         dp = [float("INF")] * (N+1)
         dp[N], dp[N-1] = 0, cost[N-1]
@@ -8,20 +14,23 @@ class Solution(object):
             dp[i] = min(cost[i]+dp[i+1], cost[i] + dp[i+2])
         
         return min(dp[0], dp[1])
-    # def minCostClimbingStairs(self, cost):
-        # """
-        # :type cost: List[int]
-        # :rtype: int
-        # """
-        # def dp(idx):
-        #     # base case
-        #     if idx == 0 or idx == 1:
-        #         return 0
+    '''
+    '''
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        def dp(idx):
+            # base case
+            if idx == 0 or idx == 1:
+                return 0
             
-        #     if idx not in cache:
-        #         cache[idx] = min(dp(idx-1)+cost[idx-1], dp(idx-2)+cost[idx-2])
-        #     return cache[idx]
+            if idx not in cache:
+                cache[idx] = min(dp(idx-1)+cost[idx-1], dp(idx-2)+cost[idx-2])
+            return cache[idx]
         
 
-        # cache = {}
-        # return dp(len(cost))
+        cache = {}
+        return dp(len(cost))
+    '''
