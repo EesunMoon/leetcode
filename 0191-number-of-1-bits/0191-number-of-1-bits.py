2f1e4714f9289ea8c9a9e18546a57eda9838a96b
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-18 16:53:00 -0500

    Time: 0 ms (100%), Space: 12.4 MB (42.74%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -7,8 +7,7 @@ class Solution(object):
         one = 0
         
         while n:
-            n, curr = divmod(n, 2)
-            if curr == 1:
-                one += 1
+            n = n & (n-1)
+            one += 1
         
         return one
