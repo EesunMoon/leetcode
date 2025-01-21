2f65c69293f1e3161a13a9389ef5fb70ae9a72f2
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-21 14:02:54 -0500

    Time: 343 ms (45.86%), Space: 12.8 MB (53.18%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -5,6 +5,23 @@ class Solution(object):
         :type coins: List[int]
         :rtype: int
         """
+        # DP solution
+        # T O(m*n), S O(m*n)
+        dp = [0] * (amount+1)
+        dp[0] = 1 # 0 amount -> 1 way
+
+        for i in range(len(coins)-1, -1, -1):
+            newDP = [0] * (amount+1)
+            newDP[0] = 1
+            for a in range(amount+1):
+                newDP[a] = dp[a]
+                if a - coins[i] >= 0:
+                    newDP[a] += newDP[a-coins[i]] # contain prior coin
+            dp = newDP
+        return dp[amount]
+        """
+        # caching
+        # T O(m*n), S O(m*n)
         dp = {}
         def dfs(i, total):
             if total == amount:
@@ -18,3 +35,4 @@ class Solution(object):
             return dp[(i, total)]
         
         return dfs(0,0)
+        """
