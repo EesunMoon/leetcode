720284fb0f8aed571a2b1c443ec32bf98702947a
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-22 11:34:32 -0500

    Time: 16 ms (68.65%), Space: 17.8 MB (51.35%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,19 +1,16 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        # sliding window
-        subSet = set()
-        l = 0
-        res = 0
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l, r = 0, 0
+        hashset = set()
+        res = 0 # length of the longest substring
 
-        for r in range(len(s)):
-            while s[r] in subSet:
-                subSet.remove(s[l])
-                l+=1
-            
-            subSet.add(s[r])
-            res = max(res, r- l +1)
+        while r < len(s):
+            # shrink window size
+            while s[r] in hashset:
+                hashset.remove(s[l])
+                l += 1
+            hashset.add(s[r])
+            r += 1
+            res = max(res, len(hashset))
+
         return res
