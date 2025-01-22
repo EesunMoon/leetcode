7dbb8992a8af5386eb5a3aac43119fdcce2c25cd
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-22 12:33:59 -0500

    Time: 320 ms (53.66%), Space: 17.8 MB (59.22%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,29 +1,30 @@
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        res, resLen = "", 0
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        maxLen = 0
+        res = ""
 
-        # check center
         for i in range(len(s)):
+            # i: consider as center
+
             # odd length
             l, r = i, i
-            while l>=0 and r<len(s) and s[l] == s[r]:
-                if r-l+1 > resLen:
-                    resLen = r-l+1
-                    res = s[l:r+1]
+            while l >=0 and r < len(s) and s[l] == s[r]:
+                length = (r-l)+1
+                if length > maxLen:
+                    maxLen = length
+                    res = s[l:l+length]
                 l -= 1
                 r += 1
             
             # even length
             l, r = i, i+1
-            while l>=0 and r<len(s) and s[l] == s[r]:
-                if r-l+1 > resLen:
-                    resLen = r-l+1
-                    res = s[l:r+1]
+            while l >=0 and r < len(s) and s[l] == s[r]:
+                length = (r-l)+1
+                if length > maxLen:
+                    maxLen = length
+                    res = s[l:l+length]
                 l -= 1
                 r += 1
 
-        return res 
+
+        return res
