77b0560e486f0a1777b1b6bf60b580c062b04784
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-23 17:01:40 -0500

    Time: 50 ms (79.3%), Space: 19.6 MB (76.82%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,23 +1,26 @@
-class Solution(object):
-    def minDistance(self, word1, word2):
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
         """
-        :type word1: str
-        :type word2: str
-        :rtype: int
+            same: (i+1, j+1)
+            differ: (1) insert: 1+ (i+1, j)
+                    (2) delete: 1+ (i, j+1)
+                    (3) replace: 1+ (i+1, j+1)
         """
-        dp = [[float("INF")] * (len(word2)+1) for _ in range(len(word1)+1)]
+        cache = {}
+        def dfs(i, j):
+            if i == len(word1):
+                return len(word2) - j
+            if j >= len(word2):
+                return len(word1) - i
 
-        # base case
-        for j in range(len(word2)+1):
-            dp[len(word1)][j] = len(word2) - j
-        for i in range(len(word1)+1):
-            dp[i][len(word2)] = len(word1) - i
+            if (i, j) in cache:
+                return cache[(i, j)]
+
+            if word1[i] == word2[j]:
+                cache[(i, j)] = dfs(i+1, j+1)
+            else:
+                cache[(i, j)] = 1 + min(dfs(i+1, j), dfs(i, j+1), dfs(i+1, j+1))
         
-        # bottom up approach
-        for i in range(len(word1)-1, -1, -1):
-            for j in range(len(word2)-1, -1, -1):
-                if word1[i] == word2[j]:
-                    dp[i][j] = dp[i+1][j+1]
-                else:
-                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j+1], dp[i+1][j+1])
-        return dp[0][0]
+            return cache[(i, j)]
+        return dfs(0,0)
+                
