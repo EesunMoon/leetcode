ee4e9e8ad486d06f7db068f1561d65039c38aedb
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-24 15:53:34 -0500

    Time: 98 ms (49.05%), Space: 27 MB (7.04%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,36 +1,13 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        """
-        ## dynamic programming approach ##
-        # base case
-        if not prices:
-            return 0
-        
-        max_profits= 0
-        min_purchase = prices[0]
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
 
-        for i in range(1, len(prices)):
-            max_profits = max(max_profits, prices[i]-min_purchase)
-            min_purchase = min(min_purchase, prices[i])
-        return max_profits
-        """
-
-        ## two pointer ## => S O(1) T O(N)
-        # initialize
-        l, r = 0, 1
-        maxProfit = 0
-
-        while r < len(prices):
-            # case1) increase
-            if prices[l] < prices[r]:
-                maxProfit=max(maxProfit, prices[r]-prices[l])
-            # case2) green
-            else:
+        # sliding window: T O(n) S O(1)
+        l = 0
+        for r in range(1, len(prices)):
+            if prices[l] > prices[r]:
                 l = r
-            r+=1
+                continue
+            res = max(res, prices[r] - prices[l])
 
-        return maxProfit
+        return res
