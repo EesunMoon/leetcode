ae227fbbc01f259b5bf1a02021d38a12a8434b08
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-24 15:45:03 -0500

    Time: 107 ms (38.28%), Space: 28.3 MB (60.95%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,22 +1,18 @@
-class Solution(object):
-    def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        # height = min(height[l], height[r])
-        # width = abs(r-l)
-
-        l, r = 0, len(height)-1 # max width
-        maxamount = 0
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        res = 0
         
+        # two pointer => T O(n), S O(1)
+        l, r = 0, len(height)-1
+
         while l<r:
-            w = abs(r-l)
-            h = min(height[l], height[r])
-            area = w*h
-            maxamount = max(maxamount, area)
+            # calculate another value
+            w, h = (r-l), min(height[l], height[r])
+            res = max(res, w*h)
+
             if height[l] < height[r]:
-                l+=1
+                l += 1
             else:
-                r-=1
-        return maxamount
+                r -= 1
+        
+        return res
