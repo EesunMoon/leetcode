dcfb63e0d7ced0e97dba3a6daa9f1a86de0ac3d7
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-24 18:03:22 -0500

    Time: 219 ms (12.33%), Space: 19 MB (33.39%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,24 +1,24 @@
-class Solution(object):
-    def minEatingSpeed(self, piles, h):
-        """
-        :type piles: List[int]
-        :type h: int
-        :rtype: int
-        """
-        l, r = 1, max(piles) # O(n)
-        minamount = r
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # base case
+        if len(piles) == h:
+            return max(piles)
+
+        l, r = 1, max(piles) # range: amount of banana to eat for a one
+        res = r
 
         while l<=r:
             m = (l+r)//2
-            
-            total = 0
-            for pile in piles:
-                total += math.ceil(float(pile)/m)
 
-            if total > h:
-                l = m+1
+            cand = 0 # candidate hour
+            for p in piles:
+                cand += math.ceil(p/float(m))
+            
+            if cand <= h:
+                res = min(res, m)
+                r = m - 1
             else:
-                minamount = min(minamount, m)
-                r = m-1
-        
-        return minamount
+                l = m + 1
+        return res
