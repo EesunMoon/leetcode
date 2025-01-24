4556f1a1a50752dc9893f1119e7516ef7be6776b
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-24 17:00:43 -0500

    Time: 129 ms (53.9%), Space: 18 MB (32.21%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,21 +1,19 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        count = {} # hashmap
-        res, mostFreq = 0, 0
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        hashmap = {}
+        maxfreq = 0
+        res = 0
+
         l = 0
         for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
-            mostFreq = max(mostFreq, count[s[r]])
+            hashmap[s[r]] = 1 + hashmap.get(s[r], 0)
+            maxfreq = max(maxfreq, hashmap[s[r]])
 
-            while (r-l+1) - mostFreq > k:
-                count[s[l]] -= 1
+            # window size - #.max frequent num = #.variable to change
+            # shrinkage condition
+            while (r-l+1) - maxfreq > k:
+                hashmap[s[l]] -= 1
                 l += 1
+            res = max(res, r-l+1)
             
-            res = max(res, r - l + 1)
         return res
-                
