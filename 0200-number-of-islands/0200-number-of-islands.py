4c6d6e58fb83b89f7feae06db3f1fc71c72e1b24
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-27 16:14:59 -0500

    Time: 245 ms (64.11%), Space: 20.3 MB (42.94%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,6 +4,8 @@ class Solution:
 
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         def search(r, c):
+            """
+            # DFS O(n*m)
             if (r < 0 or r >= ROWS or c < 0 or c >= COLS or grid[r][c] == "0"):
                 return
             
@@ -11,6 +13,23 @@ class Solution:
 
             for dr, dc in directions:
                 search(dr+r, dc+c)
+            """
+            queue = deque()
+            queue.append((r, c))
+            grid[r][c] = "0" # marked as visited
+
+            # BFS O(n*m)
+            while queue:
+                for _ in range(len(queue)):
+                    r, c = queue.popleft()
+                    for dr, dc in directions:
+                        candr, candc = dr + r, dc + c
+                        if (candr < 0 or candr >= ROWS or candc < 0 or candc >= COLS 
+                            or grid[candr][candc] == "0"):
+                            continue
+
+                        queue.append((candr, candc))
+                        grid[candr][candc] = "0"
 
         res = 0
         for r in range(ROWS):
