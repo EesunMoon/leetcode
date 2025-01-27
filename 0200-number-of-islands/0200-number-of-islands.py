f66879afe546b6f133cfd4003294c5f427ed2b49
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-27 16:12:04 -0500

    Time: 245 ms (64.11%), Space: 20.1 MB (70.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,32 +1,21 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        cnt = 0
-        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
-        def bfs(r, c):
-            queue = deque()
-            queue.append((r, c))
-            grid[r][c] = "0" # visited mark
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        def search(r, c):
+            if (r < 0 or r >= ROWS or c < 0 or c >= COLS or grid[r][c] == "0"):
+                return
+            
+            grid[r][c] = "0" # mark as visited
 
-            while queue:
-                r, c = queue.popleft()
-                for dr, dc in directions:
-                    candr, candc = dr + r, dc + c
-                    if (candr not in (-1, ROWS) and candc not in (-1, COLS)):
-                        if grid[candr][candc]== "1":
-                            queue.append((candr, candc))
-                            grid[candr][candc] = "0" # visited mark
+            for dr, dc in directions:
+                search(dr+r, dc+c)
 
-
+        res = 0
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1":
-                    bfs(r, c)
-                    cnt += 1
-        
-        return cnt
+                    search(r, c)
+                    res += 1
+        return res
