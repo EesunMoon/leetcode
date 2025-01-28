3d9972ceabdf788b1cc1ad349c99ad3bdb2eb2ec
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-28 17:32:03 -0500

    Time: 4 ms (21.23%), Space: 17.6 MB (89.74%) - LeetHub

--- 3684-substring-matching-pattern/3684-substring-matching-pattern.py
+++ 3684-substring-matching-pattern/3684-substring-matching-pattern.py
@@ -1,10 +1,19 @@
 class Solution:
     def hasMatch(self, s: str, p: str) -> bool:
         prior, after = p.split("*")
-        
+
         if not prior:
             return after in s
         
+        if not after:
+            return prior in s
+        
+        if s.find(prior) != -1:
+            if s.find(after, s.find(prior) + len(prior)) != -1:
+                return True
+        return False
+        
+        """
         # find prior string
         flag = False
         idx = 0
@@ -18,7 +27,9 @@ class Solution:
         if not flag:
             return False
         
+        # check after string
         if not after or after in s[idx:]:
             return True
         else:
             return False
+        """
