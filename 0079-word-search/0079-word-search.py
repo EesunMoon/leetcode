35992db788a73e1edd407ce62215696d3f64cb5c
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-28 17:41:18 -0500

    Time: 4728 ms (37.99%), Space: 17.9 MB (46.96%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,32 +1,27 @@
-class Solution(object):
-    def exist(self, board, word):
-        """
-        :type board: List[List[str]]
-        :type word: str
-        :rtype: bool
-        """
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLS = len(board), len(board[0])
 
-        visited = set()
-        def backtracking(r, c, i):
-            if i == len(word):
+        seen = set()
+        # back tracking
+        def dfs(r, c, idx):
+            if idx == len(word):
                 return True
-
-            if (r < 0 or r >= ROWS or c < 0 or c >= COLS 
-                or (r, c) in visited or board[r][c] != word[i]):
+            
+            if (r < 0 or r >= ROWS or c < 0 or c >= COLS or
+                (r, c) in seen or board[r][c] != word[idx]):
                 return False
             
-            visited.add((r, c))
-            res = (backtracking(r+1, c, i+1) or
-                    backtracking(r-1, c, i+1) or
-                    backtracking(r, c+1, i+1) or
-                    backtracking(r, c-1, i+1))
-            visited.remove((r,c))
-
+            seen.add((r, c))
+            res = (dfs(r+1, c, idx+1) or dfs(r-1, c, idx+1) 
+                    or dfs(r, c+1, idx+1) or dfs(r, c-1, idx+1))
+            seen.remove((r, c))
             return res
+            
         
         for r in range(ROWS):
             for c in range(COLS):
-                if backtracking(r, c, 0):
-                    return True
+                if board[r][c] == word[0]:
+                    if dfs(r, c, 0):
+                        return True
         return False
