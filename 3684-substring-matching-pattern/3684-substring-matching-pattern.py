d2223cbe18a477b64802abba2b4071e9866fdb65
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-28 17:21:35 -0500

    Time: 3 ms (37.7%), Space: 17.5 MB (98.73%) - LeetHub

---
+++ 3684-substring-matching-pattern/3684-substring-matching-pattern.py
@@ -0,0 +1,24 @@
+class Solution:
+    def hasMatch(self, s: str, p: str) -> bool:
+        prior, after = p.split("*")
+        
+        if not prior:
+            return after in s
+        
+        # find prior string
+        flag = False
+        idx = 0
+        for i in range(len(s)-len(prior)+1):
+            if s[i:i+len(prior)] == prior:
+                flag = True
+                idx = i + len(prior)
+                break
+        
+        # do not find prior string
+        if not flag:
+            return False
+        
+        if not after or after in s[idx:]:
+            return True
+        else:
+            return False
