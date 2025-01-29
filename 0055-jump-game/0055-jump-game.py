1d044f08d810ead3bf463904dd34407c00fcf99b
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-29 00:41:43 -0500

    Time: 11 ms (89.56%), Space: 18.8 MB (30.6%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,11 +1,9 @@
-class Solution(object):
-    def canJump(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
         goal = len(nums)-1
+
         for i in range(len(nums)-1, -1, -1):
-            if i + nums[i] >= goal:
+            if nums[i] + i >= goal:
                 goal = i
+        
         return goal == 0
