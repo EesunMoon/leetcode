c2e59f0768767ce297f0413766240f2c6cc3251e
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-30 17:26:20 -0500

    Time: 4 ms (1.68%), Space: 18.1 MB (51.99%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,28 +1,36 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # binary search
         l, r = 0, len(nums)-1
-
-        # binary search => O(logn)
-        while l<=r: 
-            m = (l+r)//2
-            if target == nums[m]:
-                return m
-
-            if nums[l] <= nums[m]: # left
-                if nums[m] > target and target >= nums[l]:
-                    r = m-1
-                else:
-                    l = m+1
-            else: # right
-                if nums[m] < target and target <= nums[r]:
-                    l = m+1
-                else:
-                    r = m-1
-                
-        return -1
         
+        # sorted - regular binary search
+        if nums[l] < nums[r]:
+            while l<=r:
+                m = (l+r) // 2
+                if target == nums[m]:
+                    return m
+                elif target > nums[m]:
+                    l = m + 1 # search right portion
+                else:
+                    r = m - 1 # search left portion
+        # rotated
+        else:
+            while l<=r:
+                m = (l+r)//2
+                
+                if nums[m] == target:
+                    return m
+                
+                # left portion
+                if nums[m] >= nums[l]:
+                    if target < nums[m] and target >= nums[l]:
+                        r = m - 1
+                    else:
+                        l = m + 1
+                # right portion
+                else:
+                    if target > nums[m] and target <= nums[r]:
+                        l = m + 1
+                    else:
+                        r = m - 1
+        return -1 # not found
