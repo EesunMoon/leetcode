439acdc5c33eeeec99aad7f1bc8817c779373639
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-29 21:26:36 -0500

    Time: 1 ms (8.13%), Space: 17.8 MB (50.83%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,33 +1,21 @@
-class Solution(object):
-    def rotate(self, matrix):
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
         """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
+        Do not return anything, modify matrix in-place instead.
         """
-        # set left/right boundary
         l, r = 0, len(matrix)-1
 
+        # top l -> top r -> bott r -> bott l
+        # (0, 0) -> (0, 4) -> (4, 4) -> (4, 0)
+        # (0, 1) -> (1, 4) -> (4, 3) -> (3, 0)
+
+        # (1, 1) -> (3, 1) -> (3, 3) -> (1, 3) # l = 1, r = 3
         while l<r:
-            # move element 
-            for i in range(r-l):
-                top, bottom = l, r
-
-                # save top-left element
-                topleft = matrix[top][l+i]
-
-                # move bottom-left into top-left
-                matrix[top][l+i] = matrix[bottom-i][l]
-
-                # move bottom-right into bottom-left
-                matrix[bottom-i][l] = matrix[bottom][r-i]
-
-                # move top-right into bottom-right
-                matrix[bottom][r-i] = matrix[top+i][r]
-
-                # move top-left into top-right
-                matrix[top+i][r] = topleft
-            
-            # move left, right pointers
-            l += 1
+            for c in range(r-l):
+                tmp = matrix[l][l+c] # top left
+                matrix[l][l+c] = matrix[r-c][l] # bottom left
+                matrix[r-c][l] = matrix[r][r-c] # bottom right
+                matrix[r][r-c] = matrix[l+c][r] # top right
+                matrix[l+c][r] = tmp
             r -= 1
-        
+            l += 1 
