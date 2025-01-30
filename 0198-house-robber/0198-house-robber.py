694dfd34eb62bcc29423b5daa5a8c8919c387234
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-30 14:23:26 -0500

    Time: 0 ms (100%), Space: 17.7 MB (60.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,21 +1,28 @@
-class Solution(object):
-    def rob(self, nums):
-        
-        # [DP] State Variables: index
-        #      Return: Maximum
-        def dp(idx):
-            # Base case
-            if idx == 0:
-                return nums[0]
-            elif idx == 1:
-                return max(nums[0], nums[1])
-            
-            # Recurrence Relation
-            if idx not in cache:
-                cache[idx] = max(dp(idx-1), dp(idx-2) + nums[idx])
-            return cache[idx]
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # Brute Force O(2^n) - 2 decision(contain or not)
 
-        # memoization
-        cache = {}
-        return dp(len(nums)-1) # refer to house index
-        
+        # DP - bottom up T O(n), S O(n)
+        """
+        DP = [0] * len(nums) # variable: house idx, state: maximum profit so far
+        # base case
+        DP[0], DP[1] = nums[0], max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            # recurrence relationship dp[i] = max(nums[i]+dp[i-2], dp[i-1])
+            DP[i] = max(nums[i]+DP[i-2], DP[i-1])
+        return DP[len(nums)-1]
+        """
+
+        # optimal DP - T O(n), S O(1)
+        if len(nums) == 1:
+            return nums[0]
+        # base case
+        one, two = nums[0], max(nums[0], nums[1]) # DP[0], DP[1]
+
+        for i in range(2, len(nums)):
+            # recurrence relationship dp[i] = max(nums[i]+dp[i-2], dp[i-1])
+            tmp = max(nums[i]+one, two)
+            one = two
+            two = tmp
+        return two
