7edef8fe190cd2c8e7462324a950506530bb1827
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-29 19:22:14 -0500

    Time: 580 ms (57.68%), Space: 20.7 MB (33.79%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
+        nums.sort() # O(nlogn) to perform binary search
 
-        nums.sort() # O(nlogn)
-        for k in range(len(nums)):
-            target = nums[k]
-
-            # prevent duplicate
-            if k != 0 and nums[k] == nums[k-1]:
+        for k in range(len(nums)-2):
+            # prevent duplicates
+            if k > 0 and nums[k] == nums[k-1]:
                 continue
-            
-            # binary search
-            i, j = k + 1, len(nums)-1
-            while i < j:
-                cand = target + nums[i] + nums[j]
 
-                if cand < 0:
-                    i += 1
-                elif cand > 0:
-                    j -= 1
+            # perform binary search
+            target = nums[k]
+            l, r = k+1, len(nums)-1
+            while l<r:
+                cand = target + nums[l] + nums[r]
+
+                if cand > 0:
+                    r -= 1
+                elif cand < 0:
+                    l += 1
                 else:
-                    res.append([target, nums[i], nums[j]])
-                    i += 1
-                    while i < j and nums[i] == nums[i-1]:
-                        i += 1
+                    res.append([target, nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
         return res
+            
