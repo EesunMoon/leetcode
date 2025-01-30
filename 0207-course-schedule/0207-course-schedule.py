1344be4a7ba9d7ba320313d041e74942d7dc0ff3
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-29 19:10:43 -0500

    Time: 7 ms (50.12%), Space: 18.9 MB (69.83%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,20 @@
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
-        hashset = [[] for _ in range(numCourses)]
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
         indegree = {}
         for nxt, prv in prerequisites:
-            hashset[prv].append(nxt)
+            graph[prv].append(nxt)
             indegree[nxt] = 1 + indegree.get(nxt, 0)
         
-        queue = deque(i for i in range(numCourses) if i not in indegree)
-        if not queue:
-            return False
+        queue = deque([crs for crs in range(numCourses) if crs not in indegree])
         seen = set()
         while queue:
-            crs = queue.popleft()
-            seen.add(crs)
-            for nxt in hashset[crs]:
-                indegree[nxt] -= 1
-                if indegree[nxt] == 0:
-                    queue.append(nxt)
-
+            for _ in range(len(queue)):
+                crs = queue.popleft()
+                seen.add(crs)
+                for nei in graph[crs]:
+                    indegree[nei] -= 1
+                    if indegree[nei] == 0:
+                        queue.append(nei)
+        
         return len(seen) == numCourses
