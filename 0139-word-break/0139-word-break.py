3989b5d059ed372a4ad03f38388bcf7875c9f4a9
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-30 00:52:52 -0500

    Time: 3 ms (69.77%), Space: 17.9 MB (34.68%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,27 +1,14 @@
-class Solution(object):
-    def wordBreak(self, s, wordDict):
-        """
-        :type s: str
-        :type wordDict: List[str]
-        :rtype: bool
-        """
-        
-        # DSA: DP
-        # idx : #. string
-        # state: possible or not => True, False
-        # recurrence variable: True, False
-
-        dp = [False] * (len(s)+1)
-        dp[len(s)] = True
-
-        for i in range(len(s)-1, -1, -1):
-            for w in wordDict:
-                if i + len(w) <= len(s) and s[i:i+len(w)] == w:
-                    dp[i] = dp[i+len(w)]
-                if dp[i]:
-                    break
-        
-        return dp[0]
-
-        # S O(s) s: length of the string
-        # T O(s*w) w: #.of the wordDict
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+            dp = [False] * (len(s)+1)
+            dp[len(s)] = True # base case
+            # T O(n*m) S O(n)
+            for i in range(len(s)-1, -1, -1):
+                for word in wordDict:
+                    if (i+len(word)) <= len(s) and s[i:i+len(word)] == word:
+                        dp[i] = dp[i+len(word)]
+                    
+                    if dp[i]:
+                        break
+                # print(dp)
+            return dp[0]
