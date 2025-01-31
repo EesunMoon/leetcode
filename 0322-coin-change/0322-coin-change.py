1a8c64ee91edcc789aa4a17d9f34249ee48b1313
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-31 17:35:37 -0500

    Time: 778 ms (50.88%), Space: 18.1 MB (76.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,22 +1,16 @@
-class Solution(object):
-    def coinChange(self, coins, amount):
-        """
-        :type coins: List[int]
-        :type amount: int
-        :rtype: int
-        """
-        # DSA: DP
-        # idx: amount
-        # states: #. of coins - minimum
-        # recurrence relation: dp[i] = min(dp[i], 1+dp[amo - coin])
-
-        dp = [float("inf")] * (amount+1) # bc dp[0] = 0
-        dp[0] = 0 # base case
-
-        for amo in range(1, amount+1):
-            for coin in coins:
-                if amo - coin >= 0:
-                    dp[amo] = min(dp[amo], 1+dp[amo-coin])
-        return dp[amount] if dp[amount] != float("inf") else -1
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # brute force: m**n, m = amount, n = len(coins)
+        # [dp] T O(m*n) S O(m)
+        # state: minimum number of coins, variable: amount
+        dp = [float("INF")] * (amount+1)
         
+        # base case
+        dp[0] = 0 # 0 coin use
+
+        for a in range(1, amount+1):
+            for c in coins:
+                if a-c >=0:
+                    dp[a] = min(dp[a], dp[a-c]+1)
         
+        return dp[amount] if dp[amount] != float("INF") else -1
