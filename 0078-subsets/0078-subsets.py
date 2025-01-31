bccafbd5ece06bc3d611f8bd78f6f16d7af605fb
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-31 17:23:02 -0500

    Time: 0 ms (100%), Space: 17.9 MB (50.67%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,26 +1,20 @@
-class Solution(object):
-    def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-
         subSet = []
-        def dfs(i):
-            # base case
-            if i >= len(nums):
-                res.append(subSet[::])
+        def backtracking(i):
+            if i == len(nums):
+                res.append(subSet[:])
                 return
-            
-            # option 1) include nums[i]
-            subSet.append(nums[i])
-            dfs(i+1)
 
-            # option 2) not include nums[i]
+            # contain current number
+            subSet.append(nums[i])
+            backtracking(i+1)
+
+            # not contain current number
             subSet.pop()
-            dfs(i+1)
-        
-        dfs(0)
+            backtracking(i+1)
+
+        backtracking(0)
         return res
-        
+
