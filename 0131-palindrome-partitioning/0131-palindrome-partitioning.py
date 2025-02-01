1ef538994e463594f7968f3ffd2e8a2435fc7466
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-01-31 20:38:15 -0500

    Time: 45 ms (76.4%), Space: 34.7 MB (18.12%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,30 +1,22 @@
-class Solution(object):
-    def partition(self, s):
-        """
-        :type s: str
-        :rtype: List[List[str]]
-        """
-        res = []
-        def isPalindrom(l, r):
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def isPal(l,r):
             while l<=r:
                 if s[l] != s[r]:
                     return False
-                l += 1
+                l += 1 
                 r -= 1
             return True
-        
-        def dfs(i, cand):
+        res = []
+        def backtracking(i, sub):
             if i == len(s):
-                res.append(cand[::])
+                res.append(sub[:])
                 return
+            
             for j in range(i, len(s)):
-                if isPalindrom(i, j):
-                    cand.append(s[i:j+1]) # partition
-                    dfs(j+1, cand) # next character
-                    cand.pop()
-        
-        dfs(0, [])
+                if isPal(i, j):
+                    sub.append(s[i:j+1])
+                    backtracking(j+1, sub)
+                    sub.pop()
+        backtracking(0, [])
         return res
-
-
-        
