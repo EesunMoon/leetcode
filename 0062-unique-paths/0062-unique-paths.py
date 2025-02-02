db5f84a6f93ea98f68b150529cd1f32b13c83029
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-02 15:55:47 -0500

    Time: 0 ms (100%), Space: 17.7 MB (52.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,15 +1,12 @@
-class Solution(object):
-    def uniquePaths(self, m, n):
-        """
-        :type m: int
-        :type n: int
-        :rtype: int
-        """
-        row = [1] * n # last row
-
-        for i in range(m-1):
-            currRow = [1] * n
-            for j in range(n-2, -1, -1):
-                currRow[j] = currRow[j+1] + row[j]
-            row = currRow
-        return row[0]
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # dp variable = position(index), state: #.of way
+        
+        dp = [1] * n
+        # recurrent relation: dp[r+1][c] + dp[r][c+1]
+        for r in range(m-2, -1, -1):
+            newDP = [0] * (n+1)
+            for c in range(n-1, -1, -1):
+                newDP[c] = dp[c] + newDP[c+1]
+            dp = newDP
+        return dp[0]
