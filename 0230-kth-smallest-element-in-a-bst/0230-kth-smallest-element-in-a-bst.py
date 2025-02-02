aeac583bebd26399ba269ff042c5f04b6742881f
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-01 23:29:18 -0500

    Time: 3 ms (37.8%), Space: 21.1 MB (22.26%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -1,28 +1,21 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def kthSmallest(self, root, k):
-        """
-        :type root: Optional[TreeNode]
-        :type k: int
-        :rtype: int
-        """
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # inorder => select?
         res = []
 
-        # left, parent, right
-        def inorder(root):
-            if not root:
+        def inorder(node):
+            if not node:
                 return
-            
-            inorder(root.left)
-            res.append(root.val)
-            inorder(root.right)
-        
 
+            inorder(node.left)
+            res.append(node.val)
+            inorder(node.right)
+        
         inorder(root)
         return res[k-1]
-        
