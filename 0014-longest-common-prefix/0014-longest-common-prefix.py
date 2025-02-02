d5e5bb8aa2876e9eb176a2806bd5d56c1fc34db3
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-02 13:31:16 -0500

    Time: 0 ms (100%), Space: 18.1 MB (6.75%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -2,7 +2,7 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
-
+        """
         def returnPrefix(str1, str2):
             if len(str2) > len(str1):
                 str1, str2 = str2, str1
@@ -23,3 +23,20 @@ class Solution:
                 return ""
             strs[i+1] = prefix
         return strs[len(strs)-1]
+        """
+        def isCommonPrefix(l):
+            str1 = strs[0][:l]
+            for i in range(1, len(strs)):
+                if not strs[i].startswith(str1):
+                    return False
+            return True
+
+        minLen = min(len(x) for x in strs)
+        low, high = 1, minLen
+        while low <= high:
+            middle = (low + high) // 2
+            if isCommonPrefix(middle):
+                low = middle + 1
+            else:
+                high = middle - 1
+        return strs[0][:(low+high)//2]
