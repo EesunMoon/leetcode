00233bef0e9a18b535ceec57df9e5071fed1a391
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-02 15:38:23 -0500

    Time: 326 ms (76.89%), Space: 18 MB (32.54%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -2,14 +2,17 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         # binary search
 
-        nums.sort()
+        nums.sort() # O(nlogn)
 
+        # O(n**3)
         res = []
         for i in range(len(nums)-3):
+            # prevent duplicates
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             num1 = nums[i]
             for j in range(i+1, len(nums)-2):
+                # prevent duplicates
                 if j > i + 1 and nums[j] == nums[j-1]:
                     continue
                 num2 = nums[j]
@@ -24,6 +27,7 @@ class Solution:
                     else:
                         res.append([num1, num2, nums[l], nums[r]])
                         l += 1
+                        # prevent duplicates
                         while l<r and nums[l] == nums[l-1]:
                             l += 1
         return res
