3f2235591074f0ec67d26b8303deca6b97bb35ce
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-02 16:07:31 -0500

    Time: 0 ms (100%), Space: 19.6 MB (37.48%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,6 @@
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        # XOR: if a, b is same == 1
-        res = 0 # 0 ^ n = n
-
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
         for num in nums:
-            res = res ^ num
+            res ^= num
         return res
