643106feeda4b94764559273089779029a3acc66
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-01 23:04:34 -0500

    Time: 6 ms (83.88%), Space: 18.9 MB (17.8%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -2,6 +2,7 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
         # DP : variable index state the numbere of jumps
         # T O(n*m) S O(n)
+        """
         dp = [float("INF")] * (len(nums))
         dp[len(nums)-1] = 0 # goal
 
@@ -12,3 +13,23 @@ class Solution:
                     break
                 dp[i] = min(dp[i], 1 + dp[i+j])
         return dp[0]
+        """
+        # optimize - Greedy O(n)
+        # focus on how furthest jump at each level (total level = total step)
+        
+        l, r = 0, 0 # indicate level range (base case: 0th level)
+        step = 0
+        
+        while r < len(nums)-1:
+            # how furthest jump at each level
+            furthest = 0
+            for i in range(l, r+1):
+                furthest = max(furthest, i + nums[i])
+            
+            # set next level range
+            l = r + 1
+            r = furthest
+            step += 1
+
+        return step
+            
