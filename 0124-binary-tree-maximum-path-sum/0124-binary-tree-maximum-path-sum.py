6efea628c671b75954892f8571544a46b8e6eeab
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-02 13:40:33 -0500

    Time: 14 ms (68.65%), Space: 23 MB (64.19%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -1,35 +1,27 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def maxPathSum(self, root):
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
         """
-        :type root: Optional[TreeNode]
-        :rtype: int
+            leftSubtree Max + rightSubtree Max + Root
         """
-        '''
-            two case) only one point that split path into left and right
-                1. split point: max(leftmax + rightmax + node)
-                2. other point: max(left, right) + node
-        '''
-
-        self.res = root.val
-
-        # return maximum sum of other point
+        res = [root.val]
+        # caculate maximum with subTree
         def dfs(node):
             if not node:
                 return 0
-
-            leftVal, rightVal = dfs(node.left), dfs(node.right)
-            leftVal, rightVal = max(leftVal, 0), max(rightVal, 0) # handle minus value
             
-            # compute maximum sum of split point
-            self.res = max(self.res, leftVal + rightVal + node.val)
+            leftMax, rightMax = dfs(node.left), dfs(node.right)
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
 
-            return node.val + max(leftVal, rightVal) # computer maximum sum of other point
+            res[0] = max(res[0], node.val + leftMax + rightMax) # store with Split point
+
+            return node.val + max(leftMax, rightMax) # return maximum subTree
 
         dfs(root)
-        return self.res
+        return res[0]
