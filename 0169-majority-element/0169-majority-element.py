699952fad5408d2631a4a6fba7753ed9d287fcc5
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-02 16:51:41 -0500

    Time: 0 ms (100%), Space: 19.3 MB (42.81%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,5 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        nums_set = list(set(nums))
-        k=0
-        val=nums[0]
-        for unique in nums_set:
-            if nums.count(unique) > k:
-                k=nums.count(unique)
-                val=unique
-                
-        return val
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # sorting
+        nums.sort()
+        return nums[len(nums)//2]
