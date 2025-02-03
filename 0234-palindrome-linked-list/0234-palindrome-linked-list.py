c27e050e1ef47c491f71a24d0180bd92b554ddd0
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-03 17:59:34 -0500

    Time: 30 ms (66.91%), Space: 34.9 MB (52.76%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,23 +1,35 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def isPalindrome(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: bool
-        """
-        self.front = head
-
-        def recursive(curr_node = head):
-            if curr_node is not None:
-                if not recursive(curr_node.next):
-                    return False
-                if self.front.val != curr_node.val:
-                    return False
-                self.front = self.front.next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # base case: single node and empty list
+        if not head or not head.next:
             return True
         
-        return recursive()
+        # find middle 
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # reverse second half (slow pointer)
+        prev, curr = None, slow
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+        
+        # compare
+        first, second = head, prev
+        while second:
+            if first.val != second.val:
+                return False
+            first = first.next
+            second = second.next
+        return True
+        
+
