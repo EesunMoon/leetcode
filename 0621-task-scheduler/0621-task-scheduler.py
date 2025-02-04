195119d02ae8979086f03a4e3ae332c36736783e
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-04 15:30:10 -0500

    Time: 145 ms (39.34%), Space: 19.2 MB (27.03%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,29 +1,28 @@
-class Solution(object):
-    def leastInterval(self, tasks, n):
-        """
-        :type tasks: List[str]
-        :type n: int
-        :rtype: int
-        """
-        # Heap: frequency
-        # queue: [freq, idle time]
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        # use most frequently element first <- maxHeap (freq)
+        # tracking the interval <- Queue ([freq, time])
 
-        count = Counter(tasks)
-        maxHeap = [-cnt for cnt in count.values()]
-        heapq.heapify(maxHeap)
-
+        # define hashmap O(n)
+        count = {}
+        for task in tasks:
+            count[task] = 1 + count.get(task, 0)
+        
+        # define max_heap, deque
+        max_heap = [-cnt for cnt in count.values()]
+        heapq.heapify(max_heap)
+        Q = deque() # store [freq, time to be able to use]
+        
         time = 0
-        q = deque()
-
-        while maxHeap or q:
+        while max_heap or Q:
             time += 1
-            if maxHeap:
-                cnt = 1 + heapq.heappop(maxHeap)
-                if cnt:
-                    q.append([cnt, time+n])
+            if max_heap:
+                freq = 1 + heapq.heappop(max_heap) # max heap
+                if freq:
+                    Q.append([freq, time+n]) 
             
-            if q and q[0][1] == time:
-                heapq.heappush(maxHeap, q.popleft()[0])
-        
+            # use this task
+            if Q and Q[0][1] == time:
+                freq, t = Q.popleft()
+                heapq.heappush(max_heap, freq)
         return time
-        
