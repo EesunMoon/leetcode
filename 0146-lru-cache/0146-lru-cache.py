646dedcaa73a16c996c1044c63881dda7ae14922
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-03 23:32:42 -0500

    Time: 114 ms (83.73%), Space: 78.3 MB (55.08%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,64 +1,54 @@
-class Node():
+class Node:
     def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.prev, self.next = None, None
+        self.prev = None
+        self.next = None
 
-class LRUCache(object):
+class LRUCache:
 
-    def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
+    def __init__(self, capacity: int):
+        # hashmap val:node address
+        self.hashmap = {}
+        self.head, self.tail = Node(-1, -1), Node(-1, -1)
+        self.head.next, self.tail.prev = self.tail, self.head
         self.capacity = capacity
-        self.hashmap = {} # key: address
-        self.left, self.right = Node(None, None), Node(None, None)
-        self.left.next, self.right.prev = self.right, self.left
     
-    def addNode(self, node):
-        # put in the rightmost
-        prv = self.right.prev
-        prv.next = self.right.prev = node
-        node.prev, node.next = prv, self.right
-    
-    def deleteNode(self, node):
+    def delete(self, node):
         prv, nxt = node.prev, node.next
         prv.next, nxt.prev = nxt, prv
 
-    def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
-        # if the key exist, or not
+    def add(self, node):
+        prv = self.tail.prev
+        prv.next, self.tail.prev = node, node
+        node.prev, node.next = prv, self.tail
+
+    def get(self, key: int) -> int:
         if key not in self.hashmap:
             return -1
-        
-        # update cache: delete -> add
-        target = self.hashmap[key]
-        self.deleteNode(target)
-        self.addNode(target)
-        return target.val
+        # get node -> move last index (delete, add)
+        node = self.hashmap[key]
+        self.delete(node)
+        self.add(node)
+        return node.val
 
-    def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: None
-        """
+    def put(self, key: int, value: int) -> None:
+        # existed: delete -> add (the last position)
+        # not existed: add -> check exceed capacity
         if key in self.hashmap:
-            target = self.hashmap[key]
-            self.deleteNode(target)
-        
-        new = Node(key, value)
-        self.hashmap[key] = new
-        self.addNode(new)
-        
-        if len(self.hashmap) > self.capacity:
-            # delete the least recently used key
-            lru = self.left.next
-            del self.hashmap[lru.key]
-            self.deleteNode(lru)
+            node = self.hashmap[key]
+            node.val = value # update
+            self.delete(node)
+            self.add(node)
+        else:
+            node = Node(key, value)
+            self.hashmap[key] = node
+            self.add(node)
+            if len(self.hashmap) > self.capacity:
+                LRU = self.head.next
+                del self.hashmap[LRU.key]
+                self.delete(LRU)
+
 
         
 
