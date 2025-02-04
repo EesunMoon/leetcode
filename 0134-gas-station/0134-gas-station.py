34ad391e6c9e9b8b890cb70407c9d14bf5c02091
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-03 22:22:07 -0500

    Time: 20 ms (65.96%), Space: 23.4 MB (40.93%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,24 +1,23 @@
-class Solution(object):
-    def canCompleteCircuit(self, gas, cost):
-        """
-        :type gas: List[int]
-        :type cost: List[int]
-        :rtype: int
-        """
-        # guaranteed to be unique
-        # subSum > 0 :: start
-        # total gas < total cost :: impossible
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # - cost[i-1] + gas[i]
+        # greedy: maximum gas vs minimum cost
+        # ex) gas [6, 1, 2] cost [7, 1, 1] => 1: 1, 1-1+2=2, 2-1+6 = 7
+        #    differ [-1, 0, 1]
+        # ex) gas [1, 5, 7] cost[2, 3, 5]
+        #    differ [ -1, 2, 2]
+        # ex) gas [1, 2, 3, 4, 5], cost [3 ,4 ,5, 1, 2]
+        # differ [ -2, -2, -2, 3, 3]
 
-        # base case
+        # base
         if sum(gas) < sum(cost):
             return -1
         
         total = 0
         res = 0
         for i in range(len(gas)):
-            total += (gas[i] - cost[i])
-
+            total += (-cost[i]+gas[i])
             if total < 0:
-                res = i+1 # starting from next index
                 total = 0
+                res = i + 1
         return res
