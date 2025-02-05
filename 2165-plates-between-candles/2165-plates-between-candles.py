c2ee373fe6182db4dd5ec8eec9089cab197f9e91
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-04 21:08:32 -0500

    Time: 166 ms (29.83%), Space: 48.9 MB (52.69%) - LeetHub

--- 2165-plates-between-candles/2165-plates-between-candles.py
+++ 2165-plates-between-candles/2165-plates-between-candles.py
@@ -1,61 +1,51 @@
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
-        # n: len(s), m: len(queries)
-        # BF O(m*n)
-
-        # Closet Candle position
-        # **|**|***|
-        # prefix: -1 -1 2 2 2 5 5 5 5 9 # left boundary 
-        # suffix:  2  2 2 5 5 5 9 9 9 9 # right boundary
-        # candles: 0  0 1 1 1 2 2 2 2 3
-        # => if candle: prefix == suffix
-        # [2, 5] => prefix[2] = 2, prefix[5] = 5
-        #        => suffix[2] = 2, suffix[5] = 5 
-        # :: (length - 2(candle)) => (5-2+1)-2
-        # [0, 6] => prefix[0] = -1, prefix[6] = 5
-        #        => suffix[0] =  2, suffix[6] = 9
-        #        => max(prefix[0], suffix[0]) ~ min(prefix[6], prefix[6])
-        # [3, 8] => prefix[3] = 2, prefix[8] = 5
-        #        => suffix[3] = 5, suffix[8] = 9
-
-        # S O(n) or O(m), T O(n)
+        """
+            k: len(queires), n: len(s)
+            brute force: TC O(k*n) SC O(1)
+            
+            candles: 0,  0, 1, 1, 1, 2, 2, 2, 2, 3
+            before: -1, -1, 2, 2, 2, 5, 5, 5, 5, 9 
+            after:   2,  2, 2, 5, 5, 5, 9, 9, 9, 9
+                -> if candle: before == after
+                    [2, 5] 2, 5 :: (5-2)-1
+                    [1, 6] max(-1, 2), min(5, 9) 2, 5 :: (5-2)-1
+                    [3, 7] max(2, 5), min(5, 9) 5, 5:: (5-5)-1 < -1
+                    []
+            1) store candle index in before, after array:: TC O(n), SC O(n)
+            2) compute the number of plates between candle for each query:: TC O(k)
+            =>> TC O(n+k), SC O(n)
+        """
         n = len(s)
-        prefix = [-1] * n # closet leftmost position
-        suffix = [n] * n  # closet rightmost position
+        # 1) store candle index information 
+        # 1-1) fill before array as well as prefix candle num
+        before = [-1] * (n)
         candles = [0] * n
-        # update prefix and suffix
-        candle = -1
-        totalCandle = 0
+        candle, cnt = -1, 0
         for i in range(n):
-            if s[i] == "|": 
+            if s[i] == "|":
                 candle = i
-                totalCandle += 1
-            prefix[i] = candle
-            candles[i] = totalCandle
-        
+                cnt += 1
+            before[i] = candle
+            candles[i] = cnt
+
+        # 1-2) fill after array
+        after = [n] * (n)
         candle = n
         for i in range(n-1, -1, -1):
-            if s[i] == "|": 
+            if s[i] == "|":
                 candle = i
-            suffix[i] = candle
-
-        print(prefix)
-        print(suffix)
-        res = []
-        for left, right in queries:
-            if (left == right) or (totalCandle == 0):
-                res.append(0)
-                continue          
-
-            preL, preR = prefix[left], prefix[right]
-            sufL, sufR = suffix[left], suffix[right]
-
-            left_boundary = max(preL, sufL)
-            right_boundary = min(preR, sufR)
-            numCandle = candles[right_boundary] - candles[left_boundary] - 1
-            cnt = (right_boundary-left_boundary-1) - numCandle if (right_boundary-left_boundary-1) - numCandle >= 0 else 0
-            res.append(cnt)
-
-        return res
-
+            after[i] = candle
         
+        # 2) compute the number of plates between candle for each query:: TC O(k)
+        res = []
+        for l, r in queries:
+            # base case
+            if l >= r or cnt == 0:
+                res.append(0)
+                continue
+            left, right = max(before[l], after[l]), min(before[r], after[r])
+            candleNum = candles[right] - candles[left] - 1
+            num = (right - left - 1) - candleNum if (right - left - 1) - candleNum > 0 else 0
+            res.append(num)
+        return res
