603803e0576baaed0e2b8a66424fa370ed6ccf8f
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-04 22:18:20 -0500

    Time: 28 ms (72.19%), Space: 20.3 MB (79.12%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,18 +1,35 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        # prefix & hashmap
+        """
+            prefix[i] = sum(0~i), prefix[j] = sum(0~j)
+            sum(i~j) = sum(0 ~ j) - sum(0 ~ i-1) = k (i<j)
+                => sum(0~j) - k = sum(0~i-1)
+                => prefix[j] - k = prefix[i-1]
+                => if current prefix - k is in hashmap(that store the prior prefix?)
+            hashmap: (subSum: freq)
+                base case) 0: 1
+        """
+        count = {0:1} # subSum: freq
         prefix = 0
-        cnt = 0
-        freq = {0:1}
-        """
-        sum(i, j) = prefix[j] - prefix[i-1]
-                k = prefix[j] - prefix[i-1]
-                    prefix[i-1] = prefix[j] - k
-        """
-
+        res = 0
         for num in nums:
-            prefix += num # prefix[j]
-            if prefix - k in freq:
-                cnt += freq[prefix-k]
+            prefix += num # current (prefix[j])
+            res += count.get(prefix-k, 0)
             
-            freq[prefix] = 1 + freq.get(prefix, 0) # next step: become prefix[i-1]
+            count[prefix] = 1 + count.get(prefix, 0) # store (current become prior num in next)
+        return res
+        
+        # brute force algorithm TC O(n^2) SC O(1)
+        """
+        n = len(nums)
+        cnt = 0
+        for i in range(n):
+            total = 0
+            for j in range(i, n):
+                total += nums[j]
+                if total == k:
+                    cnt += 1
+                    break
         return cnt
+        """
