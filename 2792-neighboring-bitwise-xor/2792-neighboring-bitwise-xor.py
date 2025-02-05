eeb80ad4756a117456ee06c6d1336fd70a6a53e5
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-04 20:17:31 -0500

    Time: 44 ms (58.14%), Space: 22.4 MB (60.09%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.py
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.py
@@ -3,6 +3,12 @@ class Solution:
         """
             1 => [1, 0] or [0, 1]
             0 => [1, 1] or [0, 0]
+
+            derived[0] = original[0] XOR original[1]
+            derived[1] = original[1] XOR original[2]
+            dervied[2] = original[2] XOR original[0]
+
+            => all original elements use twice
         """
         # Each derived appered one more time
         XOR = 0
