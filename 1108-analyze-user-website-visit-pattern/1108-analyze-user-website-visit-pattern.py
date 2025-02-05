209e5ed8add69d6a47901f4cdcc7b76b19b1543d
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-04 20:31:56 -0500

    Time: 12 ms (28.55%), Space: 18.6 MB (27.41%) - LeetHub

--- 1108-analyze-user-website-visit-pattern/1108-analyze-user-website-visit-pattern.py
+++ 1108-analyze-user-website-visit-pattern/1108-analyze-user-website-visit-pattern.py
@@ -1,33 +1,34 @@
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
-        # [username[i], website[i], timestamp[i]]
-        # username[i] -> website[i] in timestamp[i]
-        # home -> away -> love 
-        # home -> home -> home
-        # home -> away -> home 
-        # hashmap: key(username): value([website, timestamp])
-        # Joe: [home, 1], [about, 2], [career, 3]
-        # James: [home, 4], [cart, 5], [maps, 6], [home, 7]
-        # Mary: [home. 8], [about, 9], [career, 10]
-
-        # 1) sorted data based on timestamp
+        """
+        1. sorted by timestamp
+        2. group by username using hashmap ex) username: [pattern]
+        3. get pattern map by iterating each usergroup pattern combination (pattern length 3) 
+            ex) pattern: freq
+            => prevent duplicates in same group
+        4. return maximum score of pattern by sorting in decreasing order of 
+        """
+        
+        # 1. sorted by timestamp: TC O(nlogn) SC O(n)
         data = sorted(zip(timestamp, username, website))
 
-        # mapping username and website
-        hashmap = defaultdict(list) # username: website
+        # 2. group by username using hashmap (ex) username: [pattern]: TC O(n) SC O(n/3)
+        usermap = defaultdict(list)
         for _, name, site in data:
-            hashmap[name].append(site)
+            usermap[name].append(site)
         
-        patterns = defaultdict(int)
-        for name, site in hashmap.items():
-            seen = set()
-            for i in range(len(site)-2):
-                for j in range(i+1, len(site)-1):
-                    for k in range(j+1, len(site)):
-                        cand = tuple([site[i], site[j], site[k]])
+        # 3. get patternmap by iterating each usermap TC O(n^3) SC O(k)
+        patternmap = defaultdict(int)
+        for name, patterns in usermap.items():
+            seen = set() # prevent duplicates
+            for i in range(len(patterns)-2):
+                for j in range(i+1, len(patterns)-1):
+                    for k in range(j+1, len(patterns)):
+                        cand = tuple([patterns[i], patterns[j], patterns[k]])
                         if cand not in seen:
+                            patternmap[cand] += 1
                             seen.add(cand)
-                            patterns[cand] += 1
         
-        return sorted(patterns.keys(), key=lambda x: (-patterns[x], x))[0]
+        # 4. return maximum score of pattern
+        return sorted(patternmap.keys(), key=lambda x:(-patternmap[x], x))[0]
         
