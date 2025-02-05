47f8c6445661f842e0372bf47724ca7a803d9316
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-05 11:09:59 -0500

    Time: 51 ms (56.1%), Space: 34.6 MB (40.64%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,16 +1,24 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        def isPal(l,r):
+        """
+        s = 'aab'
+        i = 0 ~ 2
+            -> aab
+        TC O(N*2^N) SC O(N*N)
+        """
+        def isPal(l, r):
             while l<=r:
                 if s[l] != s[r]:
                     return False
-                l += 1 
+                l += 1
                 r -= 1
             return True
+        
         res = []
         def backtracking(i, sub):
+            # base case
             if i == len(s):
-                res.append(sub[:])
+                res.append(sub.copy())
                 return
             
             for j in range(i, len(s)):
@@ -18,5 +26,6 @@ class Solution:
                     sub.append(s[i:j+1])
                     backtracking(j+1, sub)
                     sub.pop()
+        
         backtracking(0, [])
         return res
