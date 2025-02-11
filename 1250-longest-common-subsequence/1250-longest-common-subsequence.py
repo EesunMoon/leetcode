ef2f70f0b1fbf7b63ef6ae2ef99dc4f38c5f48fd
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-10 23:01:00 -0500

    Time: 317 ms (96.92%), Space: 17.9 MB (93.1%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,22 +1,19 @@
-class Solution(object):
-    def longestCommonSubsequence(self, text1, text2):
-        """
-        :type text1: str
-        :type text2: str
-        :rtype: int
-        """
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # reduce space complexity: set text that has smaller size length as text2
+        # TC O(n*m) SC O(m)
         if len(text1) < len(text2):
             text1, text2 = text2, text1
-        dp = [0 for _ in range(len(text2)+1)]
-
+        
+        dp = [0] * (len(text1)+1) # act as prior dp
         for i in range(len(text1)-1, -1, -1):
-            newDP = [0 for _ in range(len(text2)+1)]
+            newDP = [0] * (len(text2)+1) # current DP
             for j in range(len(text2)-1, -1, -1):
-                # if match -> previous diagnal dp + 1
+                # 1) matches: diagnal + 1
                 if text1[i] == text2[j]:
-                    newDP[j] = 1 + dp[j+1]
-                # do not match -> max(previous down dp, current right dp)
+                    newDP[j] = dp[j+1] + 1
+                # 2) not matches: right(current DP), down(prior DP)
                 else:
-                    newDP[j] = max(dp[j], newDP[j+1])
+                    newDP[j] = max(newDP[j+1], dp[j]) # max(right, down)
             dp = newDP
         return dp[0]
