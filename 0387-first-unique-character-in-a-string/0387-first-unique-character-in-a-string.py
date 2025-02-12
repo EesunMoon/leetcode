5f78c23064918a2011c5b56dc548851631f5726e
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-11 20:38:26 -0500

    Time: 92 ms (12.6%), Space: 18.2 MB (34.65%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(len(s)):
             hashmap[s[i]] = 1 + hashmap.get(s[i], 0)
         
-        for i in range(len(s)):
-            if hashmap[s[i]] == 1:
+        for i, c in enumerate(s):
+            if hashmap[c] == 1:
                 return i
         return -1
