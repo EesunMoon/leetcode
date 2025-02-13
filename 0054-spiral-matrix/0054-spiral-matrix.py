5ab609c7b43abee5957179e261dc39ae7195a09e
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-13 13:11:39 -0500

    Time: 0 ms (100%), Space: 18 MB (18.1%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,27 +1,29 @@
-class Solution(object):
-    def spiralOrder(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[int]
-        """
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        # pattern
+        # right -> down -> left -> up
+        # [0, 1] -> [1, 0] -> [0, -1] -> [-1, 0]
 
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        ans = []
         ROWS, COLS = len(matrix), len(matrix[0])
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        diPtr = 0
+
         r, c = 0, 0
-        flag = 0
-        cnt = 0
+        res = [matrix[0][0]]
+        matrix[0][0] = "V" # mark as Visited
 
-        while cnt < ROWS * COLS:
-            ans.append(matrix[r][c])
-            matrix[r][c] = "X"
-            cnt += 1
-            candr, candc = r + directions[flag][0], c + directions[flag][1]
+        while len(res) < ROWS * COLS:
+            candr, candc = r + directions[diPtr%4][0], c + directions[diPtr%4][1]
 
-            if candr in (-1, ROWS) or candc in (-1, COLS) or matrix[candr][candc] == "X":
-                flag = (flag+1)%4
-    
-            r += directions[flag][0]
-            c += directions[flag][1]
+            # invalid element: out of bound, visited
+            if (candr < 0 or candr >= ROWS or candc < 0 or candc >= COLS 
+                or matrix[candr][candc] == "V"):
+                diPtr += 1
+                continue
+            # valid element
+            res.append(matrix[candr][candc])
+            matrix[candr][candc] = "V"
+            r, c = candr, candc
 
-        return ans
+
+        return res
