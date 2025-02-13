8e590e57594f40a3675845af20a31116f45bb1a8
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-13 16:48:46 -0500

    Time: 23 ms (17.36%), Space: 22.5 MB (14.81%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,5 +1,6 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
+        # TC O(nlogn) SC O(n)
         if not s:
             return ""
         
@@ -8,7 +9,22 @@ class Solution:
         # 1) counting the freq
         for c in s:
             hashmap[c] = 1 + hashmap.get(c, 0)
+
+        # 1-2) create bucket: index=freq, element=char
+        max_freq = max(hashmap.values())
+        bucket = [[] for _ in range(max_freq+1)]
+        for c, freq in hashmap.items():
+            bucket[freq].append(c)
         
+        # return
+        res = ""
+        for freq in range(len(bucket)-1, -1, -1):
+            for c in bucket[freq]:
+                res += (c*freq)
+        return res
+
+
+        """
         # 2) sort hashmap in decreasing freq order
         hashmap = sorted(hashmap.items(), key=lambda x:-x[1])
         
@@ -17,3 +33,4 @@ class Solution:
         for c, freq in hashmap:
             res += (c*freq)
         return res
+        """
