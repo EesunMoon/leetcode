0f84e11a991d68491b21cc2f176a22902ce149ae
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-13 16:21:57 -0500

    Time: 0 ms (100%), Space: 18.5 MB (58.5%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,28 +1,29 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def levelOrder(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # edge case
         if not root:
             return []
-
-        levels = []
-        def dfs(node, level):
-            if len(levels) == level:
-                levels.append([])
-            
-            levels[level].append(node.val)
-            if node.left:
-                dfs(node.left, 1+level)
-            if node.right:
-                dfs(node.right, 1+level)
         
-        dfs(root, 0)
-        return levels
+        # BFS using queue
+        # TC O(n) SC O(n)
+        Q = deque()
+        res = []
+        Q.append(root)
+        while Q:
+            # level
+            level = []
+            for _ in range(len(Q)):
+                node = Q.popleft()
+                level.append(node.val)
+                if node.left:
+                    Q.append(node.left)
+                if node.right:
+                    Q.append(node.right)
+            res.append(level)
+        return res
