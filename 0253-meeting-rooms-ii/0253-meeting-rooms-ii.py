39196dcf6d80d51971ea3a7e29dbb5200b727282
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-13 13:34:45 -0500

    Time: 3 ms (87.05%), Space: 19.9 MB (49.65%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,14 +1,24 @@
-class Solution(object):
-    def minMeetingRooms(self, intervals):
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        # [0, 10], [10, 15] => [0, 15]
         """
-        :type intervals: List[List[int]]
-        :rtype: int
+            1) consider earlier starting interval
+            2) consider shortest time
         """
-        intervals.sort(key=lambda x:x[0])
-        rooms = [] # min_heap
+        # [0, 17], [2, 8], [11, 13], [15, 20], [19, 21]
+        # [0, 17], [2, 21] | [0, 21], [2, 13]
+        # prior end time <= next start time:: can sum
 
-        for start, end in intervals:
-            if rooms and rooms[0] <= start:
-                heapq.heappop(rooms)
-            heapq.heappush(rooms, end)
-        return len(rooms)
+        # TC O(nlogn) SC O(n)
+        # Sorting in earlier starting time
+        intervals.sort(key=lambda x:x[0]) # sorting O(nlogn)
+        
+        H = [] # track the earliest ending time
+        heapq.heappush(H, intervals[0][1])
+
+        for i in range(1, len(intervals)):
+            # prior end time <= next start time
+            if H[0] <= intervals[i][0]:
+                heapq.heappop(H)
+            heapq.heappush(H, intervals[i][1])
+        return len(H)
