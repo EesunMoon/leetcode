6c743af6b941cd196b3ebbbea0396fc38ff0e0ea
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-14 16:55:03 -0500

    Time: 102 ms (63.08%), Space: 23.5 MB (49.85%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        # brute force: O(n**2)
+        # prefix_sum: consider 0 as -1 <<- if num.1 and n.0 is same, 0
+        # [0, 0, 1,  0, 0,0, 1, 1]
+        # [-1,-2,-1,-2,-3,-4,-3,-2]
+        # [0, 1, 0, 1]
+        # [-1,0,-1, 0]
+        # 0: idx
+        prefix_sum = 0
+        sum_idx = {0:-1}
+        res = 0
+        
+        for i, num in enumerate(nums):
+            prefix_sum += 1 if num == 1 else -1
+            if prefix_sum == 0:
+                res = i + 1
+            elif prefix_sum in sum_idx:
+                res = max(res, i - sum_idx[prefix_sum])
+            else:
+                sum_idx[prefix_sum] = i
+        return res
