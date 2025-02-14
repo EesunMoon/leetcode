ce87334e2237ec5537ce6107780830262e0d53a4
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-14 18:10:59 -0500

    Time: 46 ms (65.72%), Space: 43.2 MB (26.68%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
@@ -8,7 +8,19 @@ class Solution:
         # 2. rearrange: pos, neg, pos, neg
 
         # two pointer TC O(n) SC O(1)
-    
+        pos, neg = 0, 1
+        ans = [0] * len(nums)
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                ans[pos] = nums[i]
+                pos += 2
+            else:
+                ans[neg] = nums[i]
+                neg += 2
+        return ans
+
+
+        """
         # High-level TC O(n) SC O(n)
         if len(nums) == 0:
             return []
@@ -27,4 +39,4 @@ class Solution:
             res.append(neg[i])
         
         return res
-        
+        """
