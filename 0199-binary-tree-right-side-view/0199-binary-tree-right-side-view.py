6aafee4ca696a8da89a1cef1fe21e4bcbbd95714
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-14 01:02:07 -0500

    Time: 0 ms (100%), Space: 17.9 MB (37.08%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -1,51 +1,25 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    # BFS approach
-    def rightSideView(self, root):
-        # base case
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
-        
         res = []
-        queue = deque([root])
-        while queue:
-            # append last element at every level
-            rightmost = queue[-1].val
-            res.append(rightmost)
-
-            for _ in range(len(queue)):
-                node = queue.popleft()
+        Q = deque()
+        Q.append(root)
+        while Q:
+            level = []
+            for _ in range(len(Q)):
+                node = Q.popleft()
+                level.append(node.val)
                 if node.left:
-                    queue.append(node.left)
+                    Q.append(node.left)
                 if node.right:
-                    queue.append(node.right)
-        
+                    Q.append(node.right)
+            res.append(level[-1])
         return res
-    '''
-    # DFS approach
-    def rightSideView(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[int]
-        """
-        if not root:
-            return []
-        
-        levels = []
-        def dfs(node, level):
-            if len(levels) == level:
-                levels.append([])
-            levels[level].append(node.val)
-            if node.left:
-                dfs(node.left, level+1)
-            if node.right:
-                dfs(node.right, level+1)
-        
-        dfs(root, 0)
-        return [level[-1] for level in levels]
-    '''
+
