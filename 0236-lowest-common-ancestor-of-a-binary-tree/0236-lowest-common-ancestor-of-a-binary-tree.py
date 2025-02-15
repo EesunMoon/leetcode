da8c3a74a596eec988ff1d6cbccece3cf2176c4e
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-15 18:41:20 -0500

    Time: 54 ms (59.13%), Space: 22.1 MB (44.13%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        """
+                3
+            5       1
+         6   2    0   8
+            7  4
+        => if p is in left subtree and q is in right subtree, the split point node is LCA
+        """
+        if not root or root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        
+        return left if left else right
