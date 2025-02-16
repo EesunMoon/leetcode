96e7d2a05c7ad7fbfec536588ee45d9e49fe56ec
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-16 02:00:40 -0500

    Time: 88 ms (27.11%), Space: 18.7 MB (97.77%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -1,48 +1,15 @@
-class Solution(object):
-    def findCheapestPrice(self, n, flights, src, dst, k):
-        """
-        :type n: int
-        :type flights: List[List[int]]
-        :type src: int
-        :type dst: int
-        :type k: int
-        :rtype: int
-        """
-        # approach 2) Bellman Ford - DP
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         prices = [float("INF")] * n
         prices[src] = 0
 
-        # consider k-th stop
-        for _ in range(k+1):
-            tmpPrices = prices[:] # .copy()
+        for i in range(k+1):
+            tmpPrices = prices.copy()
 
-            for s, d, p in flights:
+            for s, d, p in flights: # source, destination, prices
                 if prices[s] == float("INF"):
                     continue
-                # update
                 if prices[s] + p < tmpPrices[d]:
-                    tmpPrices[d] = prices[s] + p
+                    tmpPrices[d] = prices[s]+p
             prices = tmpPrices
-        return prices[dst] if prices[dst]!=float("INF") else -1 
-     
-        
-        """
-        # approach 1) Dijistra
-        graph = defaultdict(list)
-        for start, end, weight in flights:
-            graph[start].append((end, weight))
-        
-        queue = [(0, 0, src)] # (cost, k, src)
-        visited = {}
-
-        while queue:
-            weight, cnt, start = heapq.heappop(queue)
-            if start == dst and cnt-1 <= k:
-                return weight
-            if start not in visited or visited[start] > cnt:
-                visited[start] = cnt
-                for new_end, new_weight in graph[start]:
-                    heapq.heappush(queue, (weight+new_weight, cnt+1, new_end))
-        
-        return -1
-        """
+        return prices[dst] if prices[dst] != float("INF") else -1
