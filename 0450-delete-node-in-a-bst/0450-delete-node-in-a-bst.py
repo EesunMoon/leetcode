a5755d4c990cded2c580c302c47e8f87ee51f4d8
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-16 01:07:55 -0500

    Time: 0 ms (100%), Space: 21.3 MB (92.71%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -1,53 +1,49 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def successor(self, root):
-        # successor: the next node, or the smallest node after the current one
-        # go to the right once, and then as many time to the left
-        root = root.right
-        while root.left:
-            root = root.left
-        return root.val
-    
-    def predecessor(self, root):
-        # predecessor: previous node in the inorder traversal
-        # go to the left once and then as many time to the right
-        root = root.left
-        while root.right:
-            root=root.right
-        return root.val
-
-    def deleteNode(self, root, key):
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         """
-        :type root: Optional[TreeNode]
-        :type key: int
-        :rtype: Optional[TreeNode]
+        edge case)
+            1. No key found
+            2. not root
+        
+        delete: 
+            not children: just delete
+            only one children: delete and connect
+            all children exist:
+                right subtree successor(the smallest value)
         """
         if not root:
             return None
-
-        # delete from right subtree
-        if key > root.val:
-            root.right = self.deleteNode(root.right, key)
-        # delete from left subtree
-        elif key < root.val:
-            root.left = self.deleteNode(root.left, key)
-        # delete from current node
-        else:
-            # 1) node is leaf
-            if not (root.left or root.right):
-                root = None
-            # 2) node has one right child
-            elif root.right:
-                root.val = self.successor(root)
-                root.right = self.deleteNode(root.right, root.val)
-            # 3) node has one right child
-            else:
-                root.val = self.predecessor(root)
-                root.left = self.deleteNode(root.left, root.val)
         
+        # find key
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            # delete node
+
+            # 1) not children
+            if not root.left and not root.right:
+                return None
+            # 2) only one children
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+            
+            # 3) all exist
+            successor = self.findMin(root.right)
+            root.val = successor.val
+            root.right = self.deleteNode(root.right, successor.val)
         return root
+
+    def findMin(self, node):
+        while node.left:
+            node = node.left
+        return node
