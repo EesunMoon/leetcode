8be3110bf8cf6c1ab52f3bbe4f8acdb9aa9327b7
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-16 01:38:06 -0500

    Time: 299 ms (75.32%), Space: 33.9 MB (22.6%) - LeetHub

---
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maximumSum(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return -1
+        
+        def getDigit(num):
+            res = 0
+            while num:
+                res += (num%10)
+                num //= 10
+            return res
+        
+
+        # digitsum: [num] -> TC O(5N) SC O(10)
+        digitMap = defaultdict(list)
+        for num in nums:
+            digit = getDigit(num)
+            heapq.heappush(digitMap[digit], num)
+            if len(digitMap[digit]) > 2:
+                heapq.heappop(digitMap[digit])
+
+        res = 0
+        for group in digitMap.values():
+            if len(group) == 2:
+                n1 = heapq.heappop(group)
+                n2 = heapq.heappop(group)
+                res = max(res, n1+n2)
+        return res if res != 0 else -1
