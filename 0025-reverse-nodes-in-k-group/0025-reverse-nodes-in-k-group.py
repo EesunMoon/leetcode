4633faff8b711ef950b49eac6b05a7ce940d8c6e
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-17 15:59:30 -0500

    Time: 0 ms (100%), Space: 18.6 MB (40.59%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -1,38 +1,37 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def reverseKGroup(self, head, k):
-        """
-        :type head: Optional[ListNode]
-        :type k: int
-        :rtype: Optional[ListNode]
-        """
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        def reversek(nodeHead):
+            previous_node = None
+            current_node = nodeHead
+            for _ in range(k):
+                next_node = current_node.next
+                current_node.next = previous_node
+                previous_node = current_node
+                current_node = next_node
+            return previous_node
+                
+        dummy = ListNode() # dummy
+        previous = dummy
         curr = head
-        newHead, tail, nextNode = self.reverseList(curr, k)
-        curr = nextNode
+
         while curr:
-            newPrev, newTail, nextNode = self.reverseList(curr, k)
-            tail.next = newPrev
-            tail = newTail
-            curr = nextNode
-
-        return newHead
-    
-    def reverseList(self, node, k):
-        prev = None
-        tail = curr = node
-        for i in range(k):
-            if i< k-1 and not curr.next:
-                return tail, None, None
-            curr = curr.next
-
-        curr = node
-        for i in range(k):
-            tmp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = tmp
-        return prev, tail, tmp # newHead, newTail, nextNode
+            nxt = curr
+            cnt = k
+            while nxt and cnt > 0:
+                nxt = nxt.next
+                cnt -= 1
+            # enough to reverse
+            if cnt == 0:
+                previous.next = reversek(curr) # return the head of reverse node
+                curr.next = nxt # connect the two set of nodes
+                previous = curr
+                curr = curr.next # move pointer
+            else:
+                break
+        
+        return dummy.next
