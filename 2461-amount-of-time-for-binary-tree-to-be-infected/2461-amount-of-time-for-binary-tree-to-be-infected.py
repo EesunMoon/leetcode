81fd6af211a3aab5c110e071c244bb6aaf2e34b2
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-18 17:00:44 -0500

    Time: 148 ms (81.48%), Space: 65.4 MB (24.66%) - LeetHub

--- 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.py
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.py
@@ -6,41 +6,119 @@
 #         self.right = right
 class Solution:
     def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
-       
-        # define adjacent list
-        graph = defaultdict(list)
+        """
+            Not using adjacent list
+            - save parent node information
+            - infection is root: max(leftDepth, rightDepth)
+
+            - infection is not root: 
+                leaf: leftDepth + rightDepth + 1 (root)
+                not leaf:
+                    if in leftSubTree
+                        max(leftDepth - depth(start~leaf)+rightDepth, depth(start~leaf))
+                            leaf node:: depth(start~leaf)
+                                        max(leftDepth+rightDepth)
+        """
+        if not root:
+            return 0
+        # 1. add parent
+        self.starting = None
+        def add_parent(curr, parent):
+            if curr:
+                curr.parent = parent
+                if curr.val == start:
+                    self.starting = curr
+                add_parent(curr.left, curr)
+                add_parent(curr.right, curr)
+        add_parent(root, None)
         
+        # 2. BFS
+        res = -1
+        seen = set([self.starting])
+        Q = deque([self.starting])
+        while Q:
+            for _ in range(len(Q)):
+                node = Q.popleft()
+                for next_node in [node.left, node.right, node.parent]:
+                    if next_node and next_node not in seen:
+                        Q.append(next_node)
+                        seen.add(next_node)
+            res += 1
+        return res
+            
+        
+
+        """
+            root = [1,5,3,null,4,10,6,9,2], start = 3
+                    infected
+            0 min: 3
+            1 min: 3, 1, 6, 10
+            2 min: 3, 1, 6, 10, 5
+            3 min: 3, 1, 6, 10, 5, 4
+            4 min: 3, 1, 6, 10, 5, 4, 9, 2 << finished
+
+            Output: #.minute needed for the entir tree to be infected
+
+            Constraint: start not in binary tree - gaurantee
+                        input Unique? - gaurantee
+
+            1. make adjacent graph: TC O(n), SC O(2n) -> O(n)
+            2. using BFS (since the node affect other node as widely as possible)
+                terminate condition) len(visited) == len(Node) or Q << O(n)
+            total TC O(n) SC O(n)
+        """
+
+        # DFS not using Adjacent Node
+        self.res = 0
+        def dfs(node):
+            if not node:
+                return -1
+
+            #search left and right subtree
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            # find the starting point
+            if node.val == start:
+                self.res = max(left, right) + 1
+                return 0
+                
+            if left >= 0:
+                self.res = max(self.res, left+right)
+
+        
+        # Using BFS and Adjacent Node TC O(n) SC O(n)
+        
+        # 1) make adjacent graph: TC O(n) SC O(n)
+        adj = defaultdict(list)
         def makeGraph(node, prev):
             if not node:
                 return
             
             if prev:
-                graph[node.val].append(prev.val)
+                adj[node.val].append(prev.val)
             if node.left:
-                graph[node.val].append(node.left.val)
+                adj[node.val].append(node.left.val)
             if node.right:
-                graph[node.val].append(node.right.val)
-            
+                adj[node.val].append(node.right.val)
+                
             makeGraph(node.left, node)
             makeGraph(node.right, node)
-        
+
         makeGraph(root, None)
-
-        if not graph:
-            return 0
-
-        # BFS
+        
+        # 2) BFS
         res = 0
-        q = deque([start])
-        seen = set()
-        seen.add(start)
+        visited = set([start])
+        Q = deque([start])
 
-        while q and len(seen) < len(graph):
-            for _ in range(len(q)):
-                node = q.popleft()
-                for nei in graph[node]:
-                    if nei not in seen:
-                        seen.add(nei) # infected
-                        q.append(nei)
+        while Q and len(visited) < len(adj):
+            for _ in range(len(Q)):
+                node = Q.popleft()
+                for nei in adj[node]:
+                    if nei not in visited:
+                        Q.append(nei)
+                        visited.add(nei)
             res += 1
+
         return res
