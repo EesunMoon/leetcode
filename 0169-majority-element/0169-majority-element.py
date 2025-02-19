d542c791ec2088919e5adb92f1b519a65129173b
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-19 14:14:06 -0500

    Time: 4 ms (70.97%), Space: 19.5 MB (19.77%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,19 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        """
+            count = 0, cand = None
+            track the nums
+            if count == 0: cand = num
+            count += 1 if cand == num else -1
+        """
+        cnt = 0
+        cand = None
+        for n in nums:
+            if cnt == 0:
+                cand = n
+            cnt += 1 if cand == n else -1
+        return cand
+        
         # sorting
         nums.sort()
         return nums[len(nums)//2]
