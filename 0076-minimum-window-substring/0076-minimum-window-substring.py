edba8a4192b56c9d679e304371d972023f2a95ce
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-02-21 15:28:10 -0500

    Time: 55 ms (92.12%), Space: 18.2 MB (77.37%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,34 +1,36 @@
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
-        countT, window = {}, {}
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # TC O(n) SC O(m)
+        # n: the length of the string s, 
+        # m: the total number of unique characters in the strings t and s
+
+        if t == "":
+            return ""
+
+        hasht, window = {}, {} # c: freq
         for c in t:
-            countT[c] = 1 + countT.get(c, 0)
+            hasht[c] = 1 + hasht.get(c, 0)
         
+        need, have = len(hasht), 0
         res, reslen = [-1, -1], float("inf")
-        need, have = len(countT), 0
         l = 0
         for r in range(len(s)):
             c = s[r]
             window[c] = 1 + window.get(c, 0)
 
-            if c in countT and countT[c] == window[c]:
+            if c in hasht and window[c] == hasht[c]:
                 have += 1
             
             while have == need:
-                # update
+                # update result
                 if (r-l+1) < reslen:
-                    res, reslen = [l, r], (r-l+1)
-
+                    res = [l, r]
+                    reslen = (r-l+1)
+                
                 # shrink
                 window[s[l]] -= 1
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                if s[l] in hasht and window[s[l]] < hasht[s[l]]:
                     have -= 1
-                l+=1
-
-        l, r = res    
+                l += 1
+        l, r = res
         return s[l:r+1] if reslen != float("inf") else ""
