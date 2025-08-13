23967d533a8e21ecfa15b9912af3c4f344169634
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-08-13 00:45:14 -0700

    Time: 23 ms (33.33%), Space: 19 MB (99.89%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,18 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        res = 0
-        # two pointer: T O(n) S O(1)
-
+        # area: min(letftMax, rightMax) - height[i]
         l, r = 0, len(height)-1
         leftMax, rightMax = height[l], height[r]
-
-        while l<r:
+        
+        res = 0
+        while l < r:
             if height[l] < height[r]:
                 l += 1
-                leftMax = max(leftMax, height[l])
-                res += (leftMax - height[l])
+                leftMax = max(height[l], leftMax)
+                res += max(0, min(leftMax, rightMax) - height[l])
             else:
-                r -= 1
-                rightMax = max(rightMax, height[r])
-                res += (rightMax - height[r])
+                r-= 1
+                rightMax = max(height[r], rightMax)
+                res += max(0, min(leftMax, rightMax) - height[r])
+            
+            
         return res
