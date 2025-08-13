5e02ad06d8fe7300d9de82b92efb8d6241d97d36
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-08-13 00:19:04 -0700

    Time: 109 ms (27.49%), Space: 28.6 MB (23.97%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,18 +1,17 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        res = 0
+        # height * width
+        # height: min(height[l], height[r]), width: r-l
+        # BF T O(n**2) | two pointer T O(n) S O(1)
         
-        # two pointer => T O(n), S O(1)
         l, r = 0, len(height)-1
-
+        res = 0
         while l<r:
-            # calculate another value
-            w, h = (r-l), min(height[l], height[r])
+            w = r-l
+            h = min(height[l], height[r])
             res = max(res, w*h)
-
             if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
-        
         return res
