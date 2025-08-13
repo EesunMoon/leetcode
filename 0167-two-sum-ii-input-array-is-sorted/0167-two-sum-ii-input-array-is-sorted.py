949a9a3753e66d4521deb8513bd8e2d59c2cc29b
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-08-12 23:18:31 -0700

    Time: 3 ms (80.82%), Space: 18.6 MB (59.47%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,20 +1,15 @@
-class Solution(object):
-    def twoSum(self, numbers, target):
-        """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # BF - all combination: T O(n^2) S O(1)
+        # binary search - T (nlogn)
+        # two pointer using combination - T O(n)
+
         l, r = 0, len(numbers)-1
-
         while l<r:
-            candidate = numbers[l] + numbers[r]
-
-            if candidate == target:
+            comb = numbers[l] + numbers[r]
+            if comb == target:
                 return [l+1, r+1]
-            elif candidate > target:
-                r-=1
+            elif comb < target:
+                l += 1
             else:
-                l+=1
-        
-        return []
+                r -= 1
