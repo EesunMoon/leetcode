210a258373c7679e89a4d03bcc0ca9fd679e5c29
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-08-19 01:27:08 -0700

    Time: 0 ms (100%), Space: 17.9 MB (32%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,71 +1,19 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        stack = []
-
-        if len(s) %2 !=0:
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # base case: length - odd number -> false
+        if len(s) % 2 != 0:
             return False
-
+        
+        stack = []
+        pair = {")":"(", "}":"{", "]":"["}
         for c in s:
-            if c in "[({":
-                stack.append(c)
-            elif not stack:
-                return False
-
-            if c in "]})":
-                if ((c == "]" and stack[-1] == "[") or 
-                    (c == "}" and stack[-1] == "{") or 
-                    (c == ")" and stack[-1] == "(")):
+            if c in "])}":
+                if stack and stack[-1] == pair[c]:
                     stack.pop()
                 else:
                     return False
-        
-        return not stack
-        
-
-
-
-
-
-        """
-        # Solution 2
-
-        stack = []
-        for c in s:
-
-            if c in "({[": # open bracket
+            else:
                 stack.append(c)
 
-            else: # close bracket
-                if not stack or (c==')' and stack[-1]!='(') or (c==']' and stack[-1]!='[') or (c=='}' and stack[-1]!='{'):
-                    return False
-                stack.pop()
-        
         return not stack
-        """
-        """
-        # Solution 1
-
-        if len(s)%2 !=0:
-            return False
-
-        # data structure - stack
-        stack = []
-        mapping = {")":"(", "}":"{", "]":"["}
-        for bracket in s:
-
-            if bracket in mapping:
-                # if bracket is closing bracket
-                top_element = stack.pop() if stack else "#"
-                
-                if mapping[bracket] != top_element:
-                    return False
-            else:
-                # if bracket is open bracket > add to stack
-                stack.append(bracket)
-        
-        return not stack
-        """
+            
