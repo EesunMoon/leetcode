aab2f932a581fc3594250ea6a51b2b5af567de6b
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-08-19 23:41:53 -0700

    Time: 0 ms (100%), Space: 18.2 MB (72.04%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,24 +1,26 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        ROWS, COLS = len(matrix), len(matrix[0]) # 3x4
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        """ 4 x 3 = m x n
+        [0,0] [0,1] [0,2] 0 1 2 [0//n, 0%n]
+        [1,0] [1,1] [1,2] 3 4 5
+        [2,0] [2,1] [2,2] 6 7 8
+        [3,0] [3,1] [3,2] 9 10 11
 
-        l, r = 0, ROWS*COLS-1
-
+        3 x 4 = m x n
+        [0,0] [0,1] [0,2] [0,3] 0 1 2 3 [0//n, 0%n]
+        [1,0] [1,1] [1,2] [1,3] 4 5 6 7
+        [2,0] [2,1] [2,2] [2,3] 8 9 10 11
+        """
+        m, n = len(matrix), len(matrix[0])
+        l, r = 0, m * n-1
         while l<=r:
-            m = (l+r)//2
-
-            # col: m // COLS
-            # row: m % COLS
-            cand = matrix[m//COLS][m%COLS]
-            if cand == target:
+            middle = (l+r)//2
+            cand = matrix[middle//n][mod(middle, n)]
+            if target == cand:
                 return True
-            elif cand > target:
-                r = m-1
-            else:
-                l = m+1
+            elif target > cand: # right boundary
+                l = middle+1
+            else: # left boundary
+                r = middle-1
+
         return False
