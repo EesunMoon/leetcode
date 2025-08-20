0ef4db3e4cdc7ba4eb45f0d3c8c1cde8f1572527
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-08-20 00:06:33 -0700

    Time: 218 ms (11.11%), Space: 19.1 MB (28.77%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -2,28 +2,23 @@ import math
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # binary search: 1 ~ max(piles)
-        # expected complexity: T O(nlogm) S O(1) n: len(piles), m: max(piles)
+
+        # T O(nlog(max(piles))) S O(1)
         
-        # set the binary search boundary
-        l, r = 1, max(piles) # O(n)
-        res = r # minimum integer
-
-        while l<=r:
-            k = (l+r)//2 # amount
-
-            # compute the time => O(n)
-            take = 0 # time
+        def calHours(k):
+            total = 0
             for p in piles:
-                take += math.ceil(p/float(k))
-            
-            # binary search
-            if take > h:
-                # k is too small to eat all bananas in h
-                l = k + 1
+                total += ceil(float(p)/k)
+            return total
+        
+        l, r = 1, max(piles) # O(n)
+        res = r
+        while l<=r:
+            k = (l+r)//2
+            cand = calHours(k)
+            if cand > h:
+                l = k+1
             else:
-                # k is big enough to eat all bananas in h
-                # find smallest value in this range
-                res = min(res, k)
-                r = k - 1
+                res = min(k, res)
+                r = k-1
         return res
