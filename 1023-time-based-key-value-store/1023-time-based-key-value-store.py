8cdecb64392cf607fe83a45ce0c05f53d9bbb213
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-08-21 01:07:36 -0700

    Time: 147 ms (27.9%), Space: 74.8 MB (36.99%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -1,41 +1,28 @@
-class TimeMap(object):
+class TimeMap:
 
     def __init__(self):
-        self.store = {} # key: [value, timestamp]        
+        self.map = defaultdict(list)
 
-    def set(self, key, value, timestamp):
-        """
-        :type key: str
-        :type value: str
-        :type timestamp: int
-        :rtype: None
-        """
-        # guarantee: timestamp - ascending order
-        if key not in self.store:
-            self.store[key] = []
-        self.store[key].append([value, timestamp])
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.map[key].append([value, timestamp])
 
-    def get(self, key, timestamp):
-        """
-        :type key: str
-        :type timestamp: int
-        :rtype: str
-        """
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.map:
+            return ""
+        
+        vals = self.map[key]
+        l, r = 0, len(vals)-1
         res = ""
-        values = self.store.get(key, [])
-
-        # binary search
-        l, r = 0, len(values)-1
         while l<=r:
             m = (l+r)//2
-            if values[m][1] <= timestamp:
-                res = values[m][0]
-                l = m + 1
+            if vals[m][1] > timestamp:
+                r = m-1
             else:
-                # invalid- timestamp cannot be smaller than target
-                r = m - 1
-        
+                res = vals[m][0]
+                l = m+1
+            
         return res
+
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
