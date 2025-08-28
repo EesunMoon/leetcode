26bc7d738b63cd9a89b96e1f8783b81e6575f2b8
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-08-28 16:12:11 -0400

    Time: 127 ms (31.92%), Space: 34.8 MB (12.39%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,17 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        """
-        ex) [73,74,75,71,69,72,76,73]
-        if stack[-1] < num => pop 
-        stack : store index
-        """
-        # stack: with monotonic decreasing stack
-        stack = [] # store index
+        # monotonic descreasing stack TC O(n) SC O(n)
+        # dp TC O(n) SC O(1) <<< ...? how?
+
+        stack = [] # [temp, idx]
         res = [0] * len(temperatures)
-        
-        for i in range(len(temperatures)-1, -1, -1):
-            while stack and temperatures[stack[-1]] <= temperatures[i]:
-                stack.pop()
-            res[i] = stack[-1] - i if stack else 0
-            stack.append(i)
+
+        for i, val in enumerate(temperatures):
+            while stack and stack[-1][0] < val:
+                temp, idx = stack.pop()
+                res[idx] = (i-idx)
+            stack.append([val, i])
         return res
