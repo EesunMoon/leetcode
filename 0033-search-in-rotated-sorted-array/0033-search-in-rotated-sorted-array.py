33717265dc14bf05fed6cf2a1b9d8daeb190f92f
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-08-28 12:29:34 -0400

    Time: 0 ms (100%), Space: 18.2 MB (22.48%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,36 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # binary search
         l, r = 0, len(nums)-1
         
-        # sorted - regular binary search
-        if nums[l] < nums[r]:
-            while l<=r:
-                m = (l+r) // 2
-                if target == nums[m]:
-                    return m
-                elif target > nums[m]:
-                    l = m + 1 # search right portion
+        while l<=r:
+            m = (l+r)//2
+            if nums[m] == target: # base case
+                return m 
+            if nums[l] < nums[r]: # standard
+                if target > nums[m]:
+                    l = m + 1
                 else:
-                    r = m - 1 # search left portion
-        # rotated
-        else:
-            while l<=r:
-                m = (l+r)//2
-                
-                if nums[m] == target:
-                    return m
-                
-                # left portion
-                if nums[m] >= nums[l]:
-                    if target < nums[m] and target >= nums[l]:
+                    r = m - 1
+            else: # rotate case
+                if nums[l] <= nums[m]:
+                    if (nums[m] > target) and (nums[l] <= target):
                         r = m - 1
                     else:
                         l = m + 1
-                # right portion
                 else:
-                    if target > nums[m] and target <= nums[r]:
+                    if (nums[m] < target) and (target <= nums[r]):
                         l = m + 1
                     else:
                         r = m - 1
-        return -1 # not found
+        return -1
