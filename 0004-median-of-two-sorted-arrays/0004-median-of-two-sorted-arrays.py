925ea9db9b257c73895ebf4b82d45b7bd0c76f2e
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-08-28 13:43:49 -0400

    Time: 0 ms (100%), Space: 18 MB (58.5%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,36 +1,34 @@
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
-        """
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        # assign small length's array as A
         A, B = nums1, nums2
         total = len(nums1) + len(nums2)
         half = total // 2
 
-        # consider the size of A is smaller than that of B
         if len(B) < len(A):
             A, B = B, A
         
+        # binary search using A (smaller length's array)
         l, r = 0, len(A)-1
         while True:
-            i = (l + r) // 2 # A
-            j = half - i - 2 # B
+            i = (l+r) // 2 # A
+            j = half - i - 2 # B (consider index starting from 0)
 
-            Aleft = A[i] if i>=0 else float("-INF")
-            Aright = A[i+1] if i+1 < len(A) else float("INF")
-            Bleft = B[j] if j>=0 else float("-INF")
-            Bright = B[j+1] if j+1 < len(B) else float("INF")
+            Aleft = A[i] if i>=0 else float("-inf") # edge case: if array is not existed
+            Aright = A[i+1] if (i+1) < len(A) else float("inf") # edge case: i+1 is out of bound
+            Bleft = B[j] if j>=0 else float("-inf")
+            Bright = B[j+1] if (j+1) < len(B) else float("inf")
 
+            # partition is correct
             if Aleft <= Bright and Bleft <= Aright:
-                # odd
-                if total % 2:
+                # case1) odd
+                if total % 2: 
                     return min(Aright, Bright)
-                return (max(Aleft, Bleft)+min(Aright,Bright))/2.0
-            elif Aleft > Bright:
-                # reduce the size of A
-                r = i-1
-            else:
-                # increase the size of A
-                l = i+1
+                # case 2) even
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2.0
+            # too many elements in A
+            elif Aleft > Bright: 
+                r = i - 1
+            # not enough elements in A
+            else: 
+                l = i + 1
