a26e79cbef6f7dedb48a02e93da14bce14e76066
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-09-15 23:58:17 -0400

    Time: 3 ms (75.08%), Space: 19.8 MB (59.45%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,7 +1,9 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         intervals.sort()
-        for i in range(1, len(intervals)):
-            if intervals[i-1][1] > intervals[i][0]:
+        endTime = float("-inf")
+        for i in range(len(intervals)):
+            if endTime > intervals[i][0]:
                 return False
+            endTime = intervals[i][1]
         return True
