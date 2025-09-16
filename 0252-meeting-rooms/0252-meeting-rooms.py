330979affeda9948628f6e51c4184d62ed80bfe2
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-09-15 23:56:25 -0400

    Time: 3 ms (75.08%), Space: 19.7 MB (59.45%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,17 +1,7 @@
-class Solution(object):
-    def canAttendMeetings(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: bool
-        """
-        if not intervals:
-            return True
-        
-        intervals.sort(key=lambda x:x[0])
-        endTime = intervals[0][1]
-        for idx in range(1, len(intervals)):
-            if endTime > intervals[idx][0]:
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort()
+        for i in range(1, len(intervals)):
+            if intervals[i-1][1] > intervals[i][0]:
                 return False
-            endTime = intervals[idx][1]
         return True
-        
