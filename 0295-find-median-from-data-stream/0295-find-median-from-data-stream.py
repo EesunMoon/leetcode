60cfad19d98bd1079eb0258b98641fba8eab8ead
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-09-25 04:03:49 -0400

    Time: 135 ms (90.84%), Space: 40.8 MB (20.56%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,30 +1,27 @@
-class MedianFinder(object):
-    # Two heap => reduce time complexity n/2
+class MedianFinder:
+
     def __init__(self):
-        self.small = [] # the smaller half of the list, max_heap
-        self.large = [] # the larger half of the list, min_heap
+        self.small = [] # maxHeap, equal or one more elem than maxHeap
+        self.large = [] # minHeap
 
-    def addNum(self, num):
-        """
-        :type num: int
-        :rtype: None
-        """
-        if len(self.small) == len(self.large):
-            heapq.heappush(self.large, -heapq.heappushpop(self.small, -num))
+    def addNum(self, num: int) -> None:
+        if self.large and self.large[0] < num:
+            heapq.heappush(self.large, num)
+            if len(self.large) > len(self.small):
+                val = heapq.heappop(self.large)
+                heapq.heappush(self.small, -val)
         else:
-            heapq.heappush(self.small, -heapq.heappushpop(self.large, num))
-        
+            heapq.heappush(self.small, -num)
+            if len(self.small)-len(self.large) > 1:
+                val = -heapq.heappop(self.small)
+                heapq.heappush(self.large, val)
 
-    def findMedian(self):
-        """
-        :rtype: float
-        """
-        if len(self.small) == len(self.large):
-            return float(self.large[0] - self.small[0]) / 2.0
+    def findMedian(self) -> float:
+        total = len(self.small) + len(self.large)
+        if total % 2 != 0:
+            return -self.small[0]
         else:
-            return float(self.large[0])
-
-        
+            return (-self.small[0] + self.large[0])/2.0
 
 
 # Your MedianFinder object will be instantiated and called as such:
