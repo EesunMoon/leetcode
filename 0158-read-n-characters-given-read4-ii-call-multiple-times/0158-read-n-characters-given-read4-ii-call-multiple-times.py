40775523a7d6e284bcc4d6db7b97d1be612b9081
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-09-30 16:49:49 -0400

    Time: 37 ms (67.51%), Space: 18 MB (23.8%) - LeetHub

---
+++ 0158-read-n-characters-given-read4-ii-call-multiple-times/0158-read-n-characters-given-read4-ii-call-multiple-times.py
@@ -0,0 +1,22 @@
+# The read4 API is already defined for you.
+# def read4(buf4: List[str]) -> int:
+
+class Solution:
+    def __init__(self):
+        self.to_load = collections.deque()
+
+    def read(self, buf: List[str], n: int) -> int:
+        cnt = 0
+        buf4 = [''] *4
+
+        while cnt < n:
+            if len(self.to_load) == 0:
+                for i in range(read4(buf4)):
+                    self.to_load.append(buf4[i])
+                if len(self.to_load) == 0:
+                    break
+            else:
+                buf[cnt] = self.to_load.popleft()
+                cnt += 1
+        return cnt
+        
