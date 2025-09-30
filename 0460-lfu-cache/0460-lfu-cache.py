c257fb249682576a9c1be663c3d3c71ea07b9d60
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-09-30 16:36:47 -0400

    Time: 125 ms (84.19%), Space: 80 MB (45.09%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.py
+++ 0460-lfu-cache/0460-lfu-cache.py
@@ -1,76 +1,46 @@
-class Node:
-    def __init__(self, key, prev=None, next=None):
-        self.key = key
-        self.prev = prev
-        self.next = next
-
-class LinkedList:
-    def __init__(self):
-        self.left, self.right = Node(0), Node(0)
-        self.left.next, self.right.prev = self.right, self.left
-        self.map = {} # val: address
-    
-    def _length(self):
-        return len(self.map)
-    
-    def pushRight(self, key): # for new Node
-        node = Node(key, self.right.prev, self.right)
-        self.map[key] = node
-        node.prev.next, self.right.prev = node, node
-
-    def pop(self, key): # remove target node
-        if key in self.map:
-            node = self.map[key]
-            prevNode, nextNode = node.prev, node.next
-            prevNode.next, nextNode.prev = nextNode, prevNode
-            self.map.pop(key, None)
-
-    def popLeft(self): # remove LRU
-        target = self.left.next.key
-        self.pop(target)
-        return target
-    
-    def update(self, key): # if using the node
-        self.pop(key)
-        self.pushRight(key)
-        
+# get & put => counter
+# LRU + LFU: [kvmap] {k: (v, f)}, [freqmap] {freq: {k:True}}
 class LFUCache:
 
     def __init__(self, capacity: int):
-        self.total = capacity
-        self.lfuCnt = 0
-        self.valMap = {} # key: val
-        self.countMap = collections.defaultdict(int) # key: count
-        self.listMap = collections.defaultdict(LinkedList) # count: LL
-
-    def counter(self, key):
-        cnt = self.countMap[key]
-        self.countMap[key] += 1
-        self.listMap[cnt].pop(key)
-        self.listMap[cnt+1].pushRight(key)
-        
-        if cnt == self.lfuCnt and self.listMap[cnt]._length() == 0:
-            self.lfuCnt += 1
+        self.cap = capacity
+        self.kv = {} # key:(val,freq)
+        self.countMap = collections.defaultdict(OrderedDict) # freq: {k:True,..}
+        self.minFreq = 0
 
     def get(self, key: int) -> int:
-        if key not in self.valMap:
+        if key not in self.kv:
             return -1
-        self.counter(key)
-        return self.valMap[key]
-
+        # update the freq
+        self.touch(key)
+        return self.kv[key][0]
+    
+    def touch(self, key):
+        val, freq = self.kv[key]
+        self.countMap[freq].pop(key)
+        self.countMap[freq+1][key] = True
+        self.kv[key] = [val, freq+1]
+        if freq == self.minFreq and len(self.countMap[freq])==0:
+            self.minFreq += 1
+        
     def put(self, key: int, value: int) -> None:
-        if self.total == 0:
+        # case 1) just update the values:: update -> touch
+        # case 2) add new val:: add -> touch
+        ## edge case 2-1) capacity:: remove lru
+        if self.cap == 0:
+            return
+        if key in self.kv:
+            self.kv[key] = [value, self.kv[key][1]]
+            self.touch(key)
             return
         
-        if key not in self.valMap and len(self.valMap) == self.total:
-            lru = self.listMap[self.lfuCnt].popLeft()
-            self.valMap.pop(lru)
-            self.countMap.pop(lru)
-
-        self.valMap[key] = value
-        self.counter(key)
-        self.lfuCnt = min(self.lfuCnt, self.countMap[key])
-
+        # edge case
+        if len(self.kv) == self.cap:
+            lru, _ = self.countMap[self.minFreq].popitem(last=False)
+            self.kv.pop(lru)
+        self.kv[key] = [value, 1]
+        self.minFreq = 1
+        self.countMap[1][key] = True
 
 # Your LFUCache object will be instantiated and called as such:
 # obj = LFUCache(capacity)
