ae1a451856b0a7d414cbde34979a789cd0fcc361
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-10-01 19:58:12 -0400

    Time: 3 ms (74.04%), Space: 25.8 MB (20.4%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -2,28 +2,23 @@ class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
-
-        1, 2, 3, 4, | 5, 6, 7, k = 3
-        5, 6, 7, | 1, 2, 3, 4 
-
-        7, 6, 5, | 4, 3, 2, 1
-
-        k = 2
         """
-        n = len(nums)
-        k = k % n
+        # [1,2,3,4,5] k=2, [4,5,1,2,3]
+        # idx = (idx+k) % len(nums)
+        # BF: T O(n*k) S O(1)
+        # k~N-1 | 0~N-k: T O(n) S O(n)
+        # [4,5,| 1,2,3]
+        # reverse 1) [5,4,| 3,2,1]
         
-        def reverseList(l, r):
-            while l<=r:
+        N = len(nums)
+        k%=N
+        def reverse(l, r):
+            while l<r:
                 nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-                r -= 1
+                l += 1; r -= 1
         
-        # 1) reverse entire array (if splitting k, we can make like moving )
-        reverseList(0, n-1)
-
-        # 2) left portion(split up to k-1th) - reverse again
-        reverseList(0, k-1)
-
-        # 3) right portion - reverse again
-        reverseList(k, n-1)
+        # 1) reverse all -> [765|4321]
+        reverse(0, N-1)
+        # 2) re-reverse 0~k-1 and k~N-1 seperately
+        reverse(0,k-1) # 5674321
+        reverse(k, N-1) # 5671234
