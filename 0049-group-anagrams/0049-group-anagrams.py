970615effe8c4bc014a8b16ab20e05ab0b539fbc
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-10-01 04:11:14 -0400

    Time: 19 ms (37.16%), Space: 22.8 MB (11.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-        hashmap = defaultdict(list) # ord count: [words]
+        Map = collections.defaultdict(list) # count: [words]
         for word in strs:
-            count = [0] * 26
-            for w in word:
-                count[ord(w) - ord('a')] += 1
-            hashmap[tuple(count)].append(word)
-        
-        return [ele for ele in hashmap.values()]
+            cnt = [0] * 26
+            for c in word:
+                cnt[ord(c)-ord('a')] += 1
+            Map[tuple(cnt)].append(word)
+
+        return [ words for words in Map.values()]
