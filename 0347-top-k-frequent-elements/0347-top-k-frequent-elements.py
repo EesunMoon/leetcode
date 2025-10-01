17a5474af589df64176bb72749d2234d3ea5658e
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-10-01 04:24:41 -0400

    Time: 4 ms (69.22%), Space: 21 MB (96.72%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,35 +1,12 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashmap = defaultdict(int)
-        """
-        max heap
-        T O(nlogn) S O(n)
+        count = {} # num: count
         for n in nums:
-            hashmap[n] -= 1 # make max heap
-
-        heap = []
-        for key, values in hashmap.items():
-            heapq.heappush(heap, [values, key])
-        ans = []
-        for i in range(k):
-            count, val = heapq.heappop(heap)
-            ans.append(val)
-        return ans
-        """
-        # min heap T O(klogk) O(n)
-        for n in nums:
-            hashmap[n] = 1 + hashmap.get(n, 0)
+            count[n] = count.get(n, 0) + 1
         
-        heap = []
-        for key, values in hashmap.items():
-            heapq.heappush(heap, [values, key])
-            while len(heap) > k:
-                heapq.heappop(heap)
-        """
-        ans = []
-        while heap:
-            count, val = heapq.heappop(heap)
-            ans.append(val)
-        return ans
-        """
-        return [val for _, val in heap]
+        minHeap = [] # [freq, num]
+        for n, freq in count.items():
+            heapq.heappush(minHeap, [freq, n])
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
+        return [ v for _, v in minHeap]
