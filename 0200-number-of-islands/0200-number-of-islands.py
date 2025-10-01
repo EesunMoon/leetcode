2f7fa3699098e34b77eb9f31fd3d00939938d85a
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-09-30 20:26:52 -0400

    Time: 244 ms (61.71%), Space: 20 MB (95.6%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,40 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
         ROWS, COLS = len(grid), len(grid[0])
+        res = 0
+        directions = [[0,1],[0,-1],[-1,0],[1,0]]
 
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        def search(r, c):
-            """
-            # DFS O(n*m)
-            if (r < 0 or r >= ROWS or c < 0 or c >= COLS or grid[r][c] == "0"):
-                return
-            
+        def bfs(r, c):
+            q = collections.deque()
+            q.append((r, c))
             grid[r][c] = "0" # mark as visited
 
-            for dr, dc in directions:
-                search(dr+r, dc+c)
-            """
-            queue = deque()
-            queue.append((r, c))
-            grid[r][c] = "0" # marked as visited
-
-            # BFS O(n*m)
-            while queue:
-                for _ in range(len(queue)):
-                    r, c = queue.popleft()
-                    for dr, dc in directions:
-                        candr, candc = dr + r, dc + c
-                        if (candr < 0 or candr >= ROWS or candc < 0 or candc >= COLS 
-                            or grid[candr][candc] == "0"):
-                            continue
-
-                        queue.append((candr, candc))
+            while q:
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    candr, candc = r+dr, c+dc
+                    if ((0<=candc<COLS) and (0<=candr<ROWS) and (grid[candr][candc] == "1")):
+                        q.append((candr, candc))
                         grid[candr][candc] = "0"
 
-        res = 0
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1":
-                    search(r, c)
+                    bfs(r, c)
                     res += 1
+
         return res
