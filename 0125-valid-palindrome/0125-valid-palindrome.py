1d2abc287beac2f9e7e63c1f87df0ef0b6b9636e
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-10-01 04:04:01 -0400

    Time: 11 ms (34.81%), Space: 18.1 MB (55.5%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,22 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # 1. ignore non-alphanumeric => using ord
-        # 2. same or not => two pointer O(n/2) -> T O(n), S O(1)
-
-        def isAlphanumeric(w):
-            return ((ord("a")<= ord(w)<=ord("z")) or
-                    (ord("A")<= ord(w)<=ord("Z")) or
-                    (ord("0")<= ord(w)<=ord("9")))
+        # alphanumeric: isalnum
         l, r = 0, len(s)-1
-        while l < r:
-            if not isAlphanumeric(s[l]) and l < r:
-                l += 1
-                continue
-            if not isAlphanumeric(s[r]) and l < r:
-                r -= 1
-                continue
-            if s[l].lower() != s[r].lower():
+        
+        while l<=r:
+            if not s[l].isalnum() and not s[r].isalnum():
+                l+=1; r-=1; continue
+            if not s[l].isalnum():
+                l += 1; continue
+            if not s[r].isalnum():
+                r -= 1; continue
+            if s[l].lower()!=s[r].lower():
                 return False
-            l += 1
-            r -= 1
+            l+=1; r-=1
         return True
