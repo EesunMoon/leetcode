908d68b2977dfef0434a6b825ac885bf8747e871
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-10-01 15:38:48 -0400

    Time: 166 ms (26.71%), Space: 77.9 MB (72.52%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,27 +1,53 @@
+class Node:
+    def __init__(self, key, val, prev=None, next=None):
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.next = next
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.Map = collections.OrderedDict()
+        self.left, self.right = Node(-1,-1), Node(-1,-1)
+        self.left.next, self.right.prev = self.right, self.left
+        self.Map = {} # key: address
+
+    def pushRight(self, node):
+        prevN, nextN = self.right.prev, self.right
+        node.prev, node.next = prevN, nextN
+        prevN.next, nextN.prev = node, node
+
+    def pop(self, node):
+        prevN, nextN = node.prev, node.next
+        prevN.next, nextN.prev = nextN, prevN
+        return node
 
     def touch(self, key):
-        val = self.Map.pop(key)
-        self.Map[key] = val
+        node = self.pop(self.Map[key])
+        self.pushRight(node)
 
     def get(self, key: int) -> int:
         if key not in self.Map:
             return -1
         self.touch(key)
-        return self.Map[key]
+        return self.Map[key].val
 
     def put(self, key: int, value: int) -> None:
         if key in self.Map:
-            self.Map[key] = value
+            self.Map[key].val = value
             self.touch(key)
             return
+
         if len(self.Map) == self.cap:
-            self.Map.popitem(last=False)
-        self.Map[key] = value
+            LRU = self.left.next
+            self.Map.pop(LRU.key)
+            self.pop(LRU)
+            
+
+        newNode = Node(key, value)
+        self.pushRight(newNode)
+        self.Map[key] = newNode
         
 
 
