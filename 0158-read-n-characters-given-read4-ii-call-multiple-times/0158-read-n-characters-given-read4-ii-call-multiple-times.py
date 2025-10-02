b72d69d817ff3a08644075824bc22399d75c9908
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-10-02 15:41:57 -0400

    Time: 41 ms (36.64%), Space: 17.8 MB (59.91%) - LeetHub

--- 0158-read-n-characters-given-read4-ii-call-multiple-times/0158-read-n-characters-given-read4-ii-call-multiple-times.py
+++ 0158-read-n-characters-given-read4-ii-call-multiple-times/0158-read-n-characters-given-read4-ii-call-multiple-times.py
@@ -1,23 +1,33 @@
 # The read4 API is already defined for you.
 # def read4(buf4: List[str]) -> int:
 
+# abcd | efgh | ijkl | mnop,  n = 3, n = 2, n=5
+# abc d => buf: abc
+# read4, fqhi: defgh
+# storage: deque
+# 1. cnt = 0 
+#### iter: if not storage -> read4 and then store
+## storage: kl < O(7)
+## buf = [fghij] cnt = 3
+
 class Solution:
     def __init__(self):
-        self.to_load = collections.deque()
-
+        self.storage = collections.deque()
     def read(self, buf: List[str], n: int) -> int:
         cnt = 0
-        buf4 = ['']*4
 
         while cnt < n:
-            if not self.to_load:
-                k = read4(buf4)
-                if k == 0: # EOF
+            if not self.storage:
+                buf4 = [""]*4
+                if read4(buf4):
+                    for v in buf4:
+                        self.storage.append(v)
+                else:
                     break
-                for i in range(k):
-                    self.to_load.append(buf4[i])
-            
-            buf[cnt] = self.to_load.popleft()
-            cnt += 1
+            else:
+                buf[cnt] = self.storage.popleft()
+                cnt += 1
+
         return cnt
+
         
