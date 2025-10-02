c2c8002aeefaf27dea6fe86ec58305d71f17c65f
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-10-02 17:57:49 -0400

    Time: 0 ms (100%), Space: 17.8 MB (65.26%) - LeetHub

--- 0362-design-hit-counter/0362-design-hit-counter.py
+++ 0362-design-hit-counter/0362-design-hit-counter.py
@@ -6,22 +6,43 @@ class HitCounter:
     # get: 300 (0~300) ->> 4
     # get: 301 (0~301) ->> 3
     # 200
+    # rid = 2
+    # [1, 3, 5, 300, 302, 401]
+    #  l.    m   r           r        
+    # gethit 300:: 0 --> -1 (return len)
+    # gethit 303:: 3 --> 1. (return len-(1+1))
+    # gethit 401:: 101 --> 
 
     def __init__(self):
         self.count = 0
-        self.q = collections.deque()
+        # self.q = collections.deque()
+        self.q = []
         
     def hit(self, timestamp: int) -> None:
         # append timestamp into the deque , count the total counts --> T O(1)
         self.q.append(timestamp)
         self.count += 1
+    
+    def binarysearch(self, timestamp):
+        l, r = 0, self.count-1
+        target = timestamp-300
+        rid = -1
+        while l<=r:
+            m = l + (r-l)//2
+            if self.q[m] > target:
+                r = m - 1
+            else:
+                rid = m
+                l = m + 1
+        return rid
 
     def getHits(self, timestamp: int) -> int:
         # remove the elements in deque, which is out out the bound (q[0] < timestamp-300)
-        while self.q and self.q[0] <= (timestamp-300):
-            self.q.popleft()
-            self.count -= 1
-        return self.count
+        # while self.q and self.q[0] <= (timestamp-300):
+        #     self.q.popleft()
+        #     self.count -= 1
+        return self.count - (self.binarysearch(timestamp)+1)
+       
 
 
         
