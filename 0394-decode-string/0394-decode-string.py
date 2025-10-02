9cd363376723805f95f4ccb2eb094351dfd6e857
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-10-02 00:54:49 -0400

    Time: 0 ms (100%), Space: 17.6 MB (97.35%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,19 +1,28 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack = []
+        # 1. [: finish digit, start s (digit shoud come out until [, after [, alpha will come out)
+        # 2. ]: can make the encoded string using stack (digitstack, strstack)
+
+        
+        # if c.isdigit(): digit = digit*10+int(c)
+        # if c == "[", append digit into digit-stack and init sub="" and digit, append sub into sub-stack
+        # if c == "]", digit pop, subsub = sub, 
+        # if c.isalpha(): sub += c
+        
+        k_stack, str_stack = [], []
+        k, curr = 0, ""
 
         for c in s:
-            if c == "]":
-                sub = ""
-                while stack[-1] !="[":
-                    sub = stack.pop() + sub
-                stack.pop() # "["
-                
-                k = ""
-                while stack and stack[-1] in "0123456789":
-                    k = stack.pop() + k
-                stack.append(sub*int(k))
+            if c.isdigit():
+                k = k*10 + int(c)
+            elif c == "[":
+                k_stack.append(k); k = 0 # finish k
+                str_stack.append(curr); curr = "" # start sub
+            elif c == "]":
+                sub = curr
+                curr = str_stack.pop()
+                cnt = k_stack.pop()
+                curr += (sub*cnt)
             else:
-                stack.append(c)
-        return "".join(stack)
-        
+                curr += c
+        return curr
