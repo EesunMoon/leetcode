2e9c32bc17329f811e293bf303f9f1dcf409ed87
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-10-02 17:15:58 -0400

    Time: 107 ms (95.94%), Space: 80 MB (60.13%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.py
+++ 0460-lfu-cache/0460-lfu-cache.py
@@ -1,46 +1,56 @@
-# get & put => counter
-# LRU + LFU: [kvmap] {k: (v, f)}, [freqmap] {freq: {k:True}}
 class LFUCache:
+    # freq: init 1 (both get put)
+    # 1) valMap --> mapping key and val {key: [val, freq]}
+    # 2) freqMap --> freq: OrderedDict{key:True}
+    # {1: {key1: true, key2: true ...},
+    #  2: {key3: true, key4: true ...},..}
+
+    # touch: update Valmap's freq, 
+    # update freqmap ->> 1) find freq using key in valMap 2) pop in freqMap, update freq (put)
+    # minF = 0 
 
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.kv = {} # key:(val,freq)
-        self.countMap = collections.defaultdict(OrderedDict) # freq: {k:True,..}
-        self.minFreq = 0
+        self.minF = 0
+        self.valMap = collections.defaultdict(list) # key: [val, freq]
+        self.freMap = collections.defaultdict(collections.OrderedDict) # freq: {key:dummy(bool)}
+
+    def touch(self, key):
+        # touch: update Valmap's freq, 
+        # update freqmap ->> 1) find freq using key in valMap 2) pop in freqMap, update freq (put)
+        _, freq = self.valMap[key]
+        self.valMap[key][1] += 1
+
+        self.freMap[freq].pop(key)
+        self.freMap[freq+1][key] = True
+
+        if len(self.freMap[self.minF]) == 0:
+            self.minF += 1
 
     def get(self, key: int) -> int:
-        if key not in self.kv:
+        # retrun val if key existed
+        if key not in self.valMap:
             return -1
-        # update the freq
         self.touch(key)
-        return self.kv[key][0]
-    
-    def touch(self, key):
-        val, freq = self.kv[key]
-        self.countMap[freq].pop(key)
-        self.countMap[freq+1][key] = True
-        self.kv[key] = [val, freq+1]
-        if freq == self.minFreq and len(self.countMap[freq])==0:
-            self.minFreq += 1
-        
+        return self.valMap[key][0] # val
+
     def put(self, key: int, value: int) -> None:
-        # case 1) just update the values:: update -> touch
-        # case 2) add new val:: add -> touch
-        ## edge case 2-1) capacity:: remove lru
-        if self.cap == 0:
-            return
-        if key in self.kv:
-            self.kv[key] = [value, self.kv[key][1]]
-            self.touch(key)
+        # if key existed: update
+        # otherwise: LRU among LFU / add, 
+        if key in self.valMap:
+            self.valMap[key][0] = value
+            self.touch(key) 
             return
         
-        # edge case
-        if len(self.kv) == self.cap:
-            lru, _ = self.countMap[self.minFreq].popitem(last=False)
-            self.kv.pop(lru)
-        self.kv[key] = [value, 1]
-        self.minFreq = 1
-        self.countMap[1][key] = True
+        if len(self.valMap) == self.cap:
+            # delete LRU among LFU
+            LRU, _ = self.freMap[self.minF].popitem(last=False)
+            self.valMap.pop(LRU)
+        
+        self.valMap[key] = [value, 1]
+        self.minF = 1
+        self.freMap[1][key] = True
+
 
 # Your LFUCache object will be instantiated and called as such:
 # obj = LFUCache(capacity)
