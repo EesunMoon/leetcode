bb8ead1f0d7cb0b403c589ddf96267f32c60e876
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-01 13:22:21 -0400

    Time: 0 ms (100%), Space: 18.5 MB (59.6%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,38 +6,22 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        """
-            Using BFS to track node as widely as possible
-                - Queue: TC O(n) SC O(w = n/2) -> O(n)
-            Using DFS through recursively
-                - recursive stack TC O(n) SC O(H = logn when balanced) -> O(logn)
-        """
-        # base case
         if not root:
             return []
-        res = [] # append level
-
-        # DFS TC O(n) SC O(H = logn when balanced) = O(N)
-        def dfs(node, level):
-            if len(res) == level:
-                res.append([]) # append level
-            res[level].append(node.val)
-            for next_node in [node.left, node.right]:
-                if next_node:
-                    dfs(next_node, level+1)
-        dfs(root, 0)
-        return res
-
-        # BFS TC O(n) SC O(W = n/2) = O(n)
-        Q = deque([root])
-        while Q:
+        q = deque()
+        q.append(root)
+        res = []
+        
+        while q:
             level = []
-            for _ in range(len(Q)):
-                node = Q.popleft()
+            for _ in range(len(q)):
+                node = q.popleft()
                 level.append(node.val)
                 if node.left:
-                    Q.append(node.left)
+                    q.append(node.left)
                 if node.right:
-                    Q.append(node.right)
+                    q.append(node.right)
             res.append(level)
         return res
+
+        
