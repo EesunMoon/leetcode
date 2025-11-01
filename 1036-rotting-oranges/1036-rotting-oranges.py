bb5da40a3b31567ff5d99eb6bad169b4ca23775a
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-01 15:55:11 -0400

    Time: 3 ms (81.41%), Space: 17.9 MB (25.91%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,43 +1,44 @@
-import collections
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        # 1. count fresh orange / add rotten in deque
-        queue = collections.deque() # save rotten coordinate
+        # 1. 0: empty, 1: fresh, 2: rotten
+        # 2. rotten spreads 4 direction -> BFS
+        # return: minimum number of minutes (all oranges are rotten), imposs->-1
+        # edge case) if there're no rotten --> -1
+        #                       no fresh --> 0
+        #            fresh is seperate from any rotten --> -1
+
+        # bfs:
+        # 1. collect original rotten oranges + count how many fresh oranges are there
+        # 2. collect adjancent fresh orange to (1) rotten oranges 
+
         fresh = 0
+        q = deque()
+        res = -1
         ROWS, COLS = len(grid), len(grid[0])
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1: # fresh
                     fresh += 1
                 elif grid[r][c] == 2: # rotten
-                    queue.append((r, c)) 
-        
-        # base case
-        if fresh == 0:
+                    q.append((r, c))
+                    grid[r][c] = 0 # visited
+        if fresh == 0: # no fresh
             return 0
-        
-        # 2. traversal BFS - count minutes , terminate if not queue
-        time = 0
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        while queue and fresh != 0:
-            for _ in range(len(queue)):
-                r, c = queue.popleft()
+        if not q: # no rotten
+            return -1
 
-                # track 4-directionally adjacent cell
+        directions = [(0,1), (0,-1),(1,0),(-1,0)]
+        while q:
+            qLen = len(q)
+            for _ in range(qLen):
+                r, c = q.popleft()
                 for dr, dc in directions:
-                    candr, candc = dr + r, dc + c
-                    
-                    # invalid condition
-                    if (candr < 0 or candr >= ROWS or candc < 0 or candc >= COLS or
-                        grid[candr][candc] != 1):
-                        continue
-                    
-                    # proceeding rotten
-                    grid[candr][candc] = 2 # marked as visited (rotten)
+                    cr, cc = dr+r, dc+c
+                    if (cr < 0 or cr >=ROWS or cc <0 or cc>=COLS or grid[cr][cc] == 0):
+                        continue # invalid or already visited or empty
+                    q.append((cr,cc))
+                    grid[cr][cc] = 0 # visited
                     fresh -= 1
-                    queue.append((candr, candc))
-            
-            time += 1
+            res += 1
 
-
-        return time if fresh == 0 else -1
+        return res if fresh == 0 else -1
