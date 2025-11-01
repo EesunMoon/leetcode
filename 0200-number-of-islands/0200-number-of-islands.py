dab48f1585576b088d2abb681b15855a8b878093
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-01 15:08:16 -0400

    Time: 233 ms (84.86%), Space: 20.1 MB (82.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        
-        ROWS, COLS = len(grid), len(grid[0])
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
+        M, N = len(grid), len(grid[0])
         res = 0
-        directions = [[0,1],[0,-1],[-1,0],[1,0]]
-
         def bfs(r, c):
-            q = collections.deque()
+            q = deque()
             q.append((r, c))
-            grid[r][c] = "0" # mark as visited
-
+            grid[r][c] = "0" # seen
             while q:
                 r, c = q.popleft()
                 for dr, dc in directions:
-                    candr, candc = r+dr, c+dc
-                    if ((0<=candc<COLS) and (0<=candr<ROWS) and (grid[candr][candc] == "1")):
-                        q.append((candr, candc))
-                        grid[candr][candc] = "0"
+                    candr, candc = dr+r, dc+c
+                    if candr <0 or candc<0 or candr>=M or candc>=N or grid[candr][candc] =="0":
+                        continue
+                    q.append((candr, candc))
+                    grid[candr][candc] = "0"
 
-        for r in range(ROWS):
-            for c in range(COLS):
+        for r in range(M):
+            for c in range(N):
                 if grid[r][c] == "1":
                     bfs(r, c)
                     res += 1
-
+        
         return res
