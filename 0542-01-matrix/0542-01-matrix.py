cc7cfda2c2a0f3ecbe0112df018a4e8b260e4eb3
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-01 16:10:15 -0400

    Time: 170 ms (39.66%), Space: 20.9 MB (37.35%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,44 +1,23 @@
-from collections import deque
-
-
-class Solution(object):
-    def updateMatrix(self, mat):
-        """
-        :type mat: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        
-        def valid(row, col):
-            return 0 <= row < m and 0 <= col < n
-        
-        # initialization
-        matrix = [row[:] for row in mat] # copy - weight matrix
-        m = len(matrix) # row
-        n = len(matrix[0]) # column
-        queue = deque()
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(mat), len(mat[0])
+        # starting from 0: dist 0
+        # add not zero (adjacent to the 0) --> dist 1
+        q = deque()
         seen = set()
-        
-        # queue: if weight is 0
-        for row in range(m):
-            for col in range(n):
-                if matrix[row][col] == 0:
-                    queue.append((row, col, 0)) # index_x, index_y, weight
-                    seen.add((row, col)) # index
-        
-        directions = [(0, 1), (1,0), (0,-1), (-1,0)] # matrix direction
-        
-        # BFS
-        while queue:
-            row, col, steps = queue.popleft()
-            
-            for dx, dy in directions:
-                next_row, next_col = row + dy, col + dx
-                
-                # if weight is not 0
-                if (next_row, next_col) not in seen and valid(next_row, next_col):
-                    seen.add((next_row, next_col))
-                    queue.append((next_row, next_col, steps+1))
-                    matrix[next_row][next_col] = steps+1
-                    
-        return matrix
-        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if mat[r][c] == 0:
+                    q.append((r,c,0)) # r, c, dist
+                    seen.add((r,c))
+        directions = [(0,1),(0,-1), (1,0), (-1,0)]
+        while q:
+            r, c, dist = q.popleft()
+            mat[r][c] = dist
+            for dr, dc in directions:
+                cr, cc = dr+r, dc+c
+                if (cr<0 or cr>=ROWS or cc<0 or cc>=COLS or (cr,cc) in seen):
+                    continue
+                q.append((cr,cc,dist+1))
+                seen.add((cr,cc))
+        return mat
