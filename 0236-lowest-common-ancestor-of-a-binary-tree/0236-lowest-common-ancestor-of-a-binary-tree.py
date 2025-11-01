0007dd0a1fc33c836aa32f4c4d6aca1ee7f508b4
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-01 14:29:48 -0400

    Time: 57 ms (38.79%), Space: 22.2 MB (20.23%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -1,14 +1,28 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-        self.parent = None
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # 1. parent map
+        # DFS
+        if (root is None) or (root is p) or (root is q):
+            return root # None or p or q
+        # search left or right
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        # p or q in both side
+        if left and right:
+            return root
+        
+        # only one side
+        return left if left else right
+
+        """ BFS 
+        # 1. parent map --> O(N), O(N)
         parMap = {root:None}
         stack = [root]
         while stack:
@@ -22,7 +36,7 @@ class Solution:
             if p in parMap and q in parMap:
                 break
         
-        # 2. p_parent
+        # 2. p_parent --> O(H)
         cand = set()
         while p:
             cand.add(p)
@@ -35,3 +49,4 @@ class Solution:
             q = parMap[q]
         
         return None
+        """
