74dacaef067fa6f1cf6022d3c9ce85776baec983
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-01 20:01:14 -0400

    Time: 3 ms (87.77%), Space: 19.1 MB (72.68%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,20 +1,23 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = defaultdict(list)
-        indegree = {}
+        ## [a,b]: b-->a
+        ## Node: 0~numCourses
+
+        # 1. adj list T O(m) S O(n)
+        adj = defaultdict(list)
+        indegree = [0]*numCourses
         for nxt, prv in prerequisites:
-            graph[prv].append(nxt)
-            indegree[nxt] = 1 + indegree.get(nxt, 0)
+            adj[prv].append(nxt)
+            indegree[nxt] += 1
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
         
-        queue = deque([crs for crs in range(numCourses) if crs not in indegree])
-        seen = set()
-        while queue:
-            for _ in range(len(queue)):
-                crs = queue.popleft()
-                seen.add(crs)
-                for nei in graph[crs]:
-                    indegree[nei] -= 1
-                    if indegree[nei] == 0:
-                        queue.append(nei)
-        
-        return len(seen) == numCourses
+        while q:
+            crs = q.popleft()
+            for nei in adj[crs]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        return True if sum(indegree) == 0 else False
