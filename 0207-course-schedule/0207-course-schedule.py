43c349908535a9822da339a136fc9c858fb4a714
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-01 20:03:14 -0400

    Time: 3 ms (87.77%), Space: 18.8 MB (98.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -4,20 +4,23 @@ class Solution:
         ## Node: 0~numCourses
 
         # 1. adj list T O(m) S O(n)
-        adj = defaultdict(list)
+        adj = [[] for _ in range(numCourses)]
         indegree = [0]*numCourses
         for nxt, prv in prerequisites:
             adj[prv].append(nxt)
             indegree[nxt] += 1
+        
+        # 2. indegree == 0
         q = deque()
         for i in range(numCourses):
             if indegree[i] == 0:
                 q.append(i)
         
+        # 3. bfs
         while q:
             crs = q.popleft()
             for nei in adj[crs]:
                 indegree[nei] -= 1
                 if indegree[nei] == 0:
                     q.append(nei)
-        return True if sum(indegree) == 0 else False
+        return sum(indegree) == 0
