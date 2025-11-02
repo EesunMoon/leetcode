5a0fe735128121a0a77604bc2ddfa32517362ca3
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-02 01:56:40 -0400

    Time: 55 ms (68.84%), Space: 32.9 MB (22.58%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,19 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # brute force O(n^2) => for all subarray
-        # [-2,1,-3,4,-1,2,1,-5,4]
-        # subSum < 0
-        # [-2, -1, -2, -1, -2, -1, -2, -1]
-        # sliding window
-
-        l = 0
-        subSum = 0
-        maxNum = nums[0]
-        for r in range(len(nums)):
-            subSum += nums[r]
-            maxNum = max(subSum, maxNum)
-            if subSum < nums[r]:
-                maxNum = max(maxNum, nums[r])
-                l = r
-                subSum = nums[r]
-        return maxNum
+        maxSub = nums[0]
+        currSum = 0
+        for n in nums:
+            if currSum < 0:
+                currSum = 0
+            currSum += n
+            maxSub = max(maxSub, currSum)
+        return maxSub
