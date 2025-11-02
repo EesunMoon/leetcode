40292f5b10fb367c3b3a1326e0f365287792ae69
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-01 20:37:16 -0400

    Time: 53 ms (5.76%), Space: 18.2 MB (56.95%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,33 +1,23 @@
 """
 # Definition for a Node.
-class Node(object):
+class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-class Solution(object):
-    def cloneGraph(self, node):
-        """
-        :type node: Node
-        :rtype: Node
-        """
-        graph={} # Node address: Node
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        oldToNew = {} # node: copy
 
-        def clone(curr):
-            # already in graph hashmap
-            if curr in graph:
-                return graph[curr]
-            
-            # create new node
-            new = Node(curr.val)
-            graph[curr] = new
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node] # address
+            newNode = Node(node.val)
+            oldToNew[node] = newNode
+            for nei in node.neighbors:
+                newNode.neighbors.append(dfs(nei))
+            return newNode
 
-            # connect neighbors to new(clone) node
-            for nei in curr.neighbors:
-                new.neighbors.append(clone(nei))
-            
-            return new
-
-        return clone(node) if node else None
-        
+        return dfs(node) if node else None
