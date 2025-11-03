6a34ca24c4fe2534524e87867ecbf63711b094fa
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-03 01:09:00 -0500

    Time: 135 ms (77.46%), Space: 32.2 MB (63.51%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -1,44 +1,25 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    # approach 2) not using global variable (same time complexity)
-    def goodNodes(self, root):
-        def dfs(node, maxVal):
-            if not node:
-                return 0
-            res = 1 if node.val >= maxVal else 0
-            maxVal = max(maxVal, node.val)
-            res += dfs(node.left, maxVal)
-            res += dfs(node.right, maxVal)
-            return res
-        return dfs(root, root.val)
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        res = 1 # root is always considered as good.
+        q = deque() # [maximum value so far, node]
+        q.append((root.val, root))
+        while q:
+            maxVal, node = q.popleft()
+            if node.left:
+                if node.left.val >= maxVal:
+                    res += 1
+                q.append([max(node.left.val, maxVal), node.left])
+            if node.right:
+                if node.right.val >= maxVal:
+                    res += 1
+                q.append([max(node.right.val, maxVal), node.right])
+        return res
 
-    '''
-    # approach 1) using global variable
-    def goodNodes(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        self.res = 1 # contain root node
-
-        def dfs(node, maxVal):
-            if not node:
-                return None
-            
-            maxVal = max(maxVal, node.val)
-            if node.left and maxVal <= node.left.val:
-                self.res += 1
-            if node.right and maxVal <= node.right.val:
-                self.res +=1
-            
-            dfs(node.left, maxVal)
-            dfs(node.right, maxVal)
-
-        dfs(root, root.val)
-        return self.res
-    '''
