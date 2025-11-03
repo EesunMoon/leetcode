e67197d0bb4711d371163a1aa3acbf02d0dd0b38
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-03 03:00:02 -0500

    Time: 3 ms (87.29%), Space: 19.3 MB (70.89%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -8,11 +8,28 @@ class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         # preorder: curr, left, right (first value is always root)
         # inorder: left, curr, right (left index boundary, right index boundary)
-        if not preorder or not inorder:
-            return None
-        rootVal, rootIdx = preorder[0], inorder.index(preorder[0])
-        root = TreeNode(rootVal)
+        inorder_indices = {val: idx for idx, val in enumerate(inorder)}
 
-        root.left = self.buildTree(preorder[1:rootIdx+1], inorder[:rootIdx])
-        root.right = self.buildTree(preorder[rootIdx+1:], inorder[rootIdx+1:])
-        return root
+        self.preorder_idx = 0
+        def dfs(l, r):
+            if l > r:
+                return None
+            root_val = preorder[self.preorder_idx]
+            self.preorder_idx += 1
+            root = TreeNode(root_val)
+
+            mid = inorder_indices[root_val]
+            root.left = dfs(l, mid-1)
+            root.right = dfs(mid+1, r)
+            return root
+
+        return dfs(0, len(inorder)-1)
+
+        # if not preorder or not inorder:
+        #     return None
+        # rootVal, rootIdx = preorder[0], inorder.index(preorder[0])
+        # root = TreeNode(rootVal)
+
+        # root.left = self.buildTree(preorder[1:rootIdx+1], inorder[:rootIdx])
+        # root.right = self.buildTree(preorder[rootIdx+1:], inorder[rootIdx+1:])
+        # return root
