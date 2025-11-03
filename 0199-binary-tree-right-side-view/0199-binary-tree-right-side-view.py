3a827ec4c152347e3a07883b1532a431beb9a878
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-03 00:52:36 -0500

    Time: 0 ms (100%), Space: 17.9 MB (17.21%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -8,52 +8,15 @@ class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
-
+        q = deque([root])
         res = []
-        """
-            DFS TC O(n) SC O(H)
-        """
-        def dfs(node, level):
-            if len(res) == level:
-                res.append(node.val)
-            """
-                if we want to left side: [node.left, node.right]
-                in this question we want to save right side => [node.right, node.left]
-            """
-            for next_node in [node.right, node.left]: # we want to see right side first
-                if next_node:
-                    dfs(next_node, level+1)
-        dfs(root, 0)
+        while q:
+            qLen = len(q)
+            res.append(q[0].val)
+            for _ in range(qLen):
+                curr = q.popleft()
+                if curr.right:
+                    q.append(curr.right)
+                if curr.left:
+                    q.append(curr.left)
         return res
-
-
-        """
-            BFS TC O(n) SC O(W = n/2)
-        """
-        # not using additional level array
-        Q = deque([root])
-        while Q:
-            for _ in range(len(Q)):
-                node = Q.popleft()
-                if node.left:
-                    Q.append(node.left)
-                if node.right:
-                    Q.append(node.right)
-            res.append(node.val)
-        return res
-        """
-        ## Two list
-        Q = deque()
-        Q.append(root)
-        while Q:
-            level = []
-            for _ in range(len(Q)):
-                node = Q.popleft()
-                level.append(node.val)
-                if node.left:
-                    Q.append(node.left)
-                if node.right:
-                    Q.append(node.right)
-            res.append(level[-1])
-        return res
-        """
