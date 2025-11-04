3e121e109c0005bdf95175f7849a1683e057b86c
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-04 00:51:42 -0500

    Time: 772 ms (13.35%), Space: 20.4 MB (82.23%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.py
+++ 1558-course-schedule-iv/1558-course-schedule-iv.py
@@ -1,33 +1,30 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        # prerequisite (a, b) a -> b
-        # query (a, b) a -> b
-
-        # make adjacent graph TC O(N) SC O(N^2)
-        indegree = {} # course: indegree
-        adj = defaultdict(list)
-        for pre, nxt in prerequisites:
-            adj[pre].append(nxt)
-            indegree[nxt] = 1 + indegree.get(nxt,0)
+        # courses node: 0~numCourses-1
+        # prerequisites: [pre, nxt]
+        # queries: [pre -> nxt?]
+        adj = defaultdict(set)
+        for prev, nxt in prerequisites:
+            adj[prev].add(nxt)
         
-        # TC O(N^3) SC O(N^2)
-        premap = defaultdict(set) # nxt: (pre)
-        Q = deque([crs for crs in range(numCourses) if crs not in indegree])
-        while Q:
-            crs = Q.popleft()
-            for nxt in adj[crs]:
-                premap[nxt].add(crs)
-                for pre in premap[crs]:
-                    premap[nxt].add(pre)
-                indegree[nxt] -= 1
-                if indegree[nxt] == 0:
-                    Q.append(nxt)
-
-        # mapping through premap TC O(Q) Q:len(queries)
+        def bfs(u, v):
+            if not u in adj:
+                return False
+            q = deque([u])
+            visited = set()
+            visited.add(u)
+            while q:
+                curr = q.popleft()
+                if v in adj[curr]:
+                    return True
+                for nxt in adj[curr]:
+                    if nxt not in visited:
+                        q.append(nxt)
+                        visited.add(nxt)
+            return False
         res = []
-        for pre, nxt in queries:
-            if pre in premap[nxt]:
-                res.append(True)
-            else:
-                res.append(False)
+        for u, v in queries:
+            res.append(bfs(u,v))
         return res
+
+            
