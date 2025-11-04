618ae391eb50e9b9ed58b1e82824e434151665cb
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-04 02:12:34 -0500

    Time: 23 ms (54.82%), Space: 18.4 MB (40.65%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -2,13 +2,17 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
         maxArea = 0
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
 
         def dfs(r, c):
             if r < 0 or r >= m or c < 0 or c >= n or grid[r][c] == 0:
                 return 0
             
             grid[r][c] = 0 # visited
-            return (1+dfs(r-1,c)+dfs(r+1,c)+dfs(r,c-1)+dfs(r,c+1))
+            cnt = 1
+            for dr, dc in directions:
+                cnt += dfs(dr+r, dc+c)
+            return cnt
 
         for r in range(m):
             for c in range(n):
