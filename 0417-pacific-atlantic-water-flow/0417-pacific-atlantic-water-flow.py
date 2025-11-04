8a306e0bcc7c43826792cb595c3ffdee6c09b3a3
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-04 14:30:55 -0500

    Time: 40 ms (26.99%), Space: 19.6 MB (22.02%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,38 +1,40 @@
-class Solution(object):
-    def pacificAtlantic(self, heights):
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         """
-        :type heights: List[List[int]]
-        :rtype: List[List[int]]
+        BF: every cell - DFS/BFS for pacific & atlantic 
+            current cell height >= neighbor's cell height
+            TC O(2(N*M)^2)
+        Optimal: starting from pacific & atlantic - DFS/BFS 
+            to check where ocean cell can reach
+            duplicates --> result
+            current cell height <= neighbor's cell height
+            TC O(N*M)
         """
-        ROWS, COLS = len(heights), len(heights[0])
-        pac, atl = set(), set() # marked as visited
+        m, n = len(heights), len(heights[0])
+        pacific, atlantic = set(), set()
+        # pacific bound: row = 0 OR col = 0
+        # atlantic bound: row = m-1 OR col = n-1
 
-        def dfs(r, c, seen, preHeights):
-            # base case
-            if r in (-1, ROWS) or c in (-1, COLS) or (r, c) in seen or heights[r][c] < preHeights:
+        def dfs(r, c, visit, prevHeight):
+            if ((r,c) in visit or r<0 or r>=m or c<0 or c>=n
+                or heights[r][c] < prevHeight):
                 return
-            
-            seen.add((r, c))
-            
-            dfs(r-1, c, seen, heights[r][c])
-            dfs(r+1, c, seen, heights[r][c])
-            dfs(r, c-1, seen, heights[r][c])
-            dfs(r, c+1, seen, heights[r][c])
-            
-        # search
-        for r in range(ROWS):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, COLS-1, atl, heights[r][COLS-1])
+            visit.add((r,c))
+            dfs(r-1,c, visit, heights[r][c])
+            dfs(r+1,c, visit, heights[r][c])
+            dfs(r,c-1, visit, heights[r][c])
+            dfs(r,c+1, visit, heights[r][c])
 
-        for c in range(COLS):
-            dfs(0, c, pac, heights[0][c])
-            dfs(ROWS-1, c, atl, heights[ROWS-1][c])
-
-        # result
+        for c in range(n):
+            dfs(0, c, pacific, heights[0][c]) # pacific - first row
+            dfs(m-1, c, atlantic, heights[m-1][c]) # pacific - last row
+        for r in range(m):
+            dfs(r,0, pacific, heights[r][0]) # pacific - first column
+            dfs(r, n-1, atlantic, heights[r][n-1]) # pacific - last column
+        
         res = []
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r, c) in pac and (r, c) in atl:
-                    res.append([r, c])
-
+        for r in range(m):
+            for c in range(n):
+                if (r,c) in pacific and (r,c) in atlantic:
+                    res.append([r,c])
         return res
