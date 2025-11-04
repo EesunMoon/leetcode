c4ecf8132c4d5e929ef6446dd45e6f223dcac09f
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-04 04:15:41 -0500

    Time: 3 ms (86.73%), Space: 21.6 MB (88.07%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,38 +1,33 @@
-class Solution(object):
-    def solve(self, board):
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
         """
-        :type board: List[List[str]]
-        :rtype: None Do not return anything, modify board in-place instead.
+        Do not return anything, modify board in-place instead.
         """
-        ROWS, COLS = len(board), len(board[0])
+        m, n = len(board), len(board[0])
 
-        # 1) [DFS] unsurrounding "O" -> "V"
+        # 1. unsurrounded portion: O -> T
         def dfs(r, c):
-            # base case
-            if r in (-1, ROWS) or c in (-1, COLS) or board[r][c] != "O":
+            if (r<0 or r>=m or c<0 or c>=n or board[r][c]!="O"):
                 return
-            board[r][c] = "V"
-            
-            # direction
+            board[r][c] = "T"
             dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c+1)
-            dfs(r, c-1)
-        
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "O" and r in [0, ROWS-1] or c in [0, COLS-1]:
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+            
+        for r in range(m):
+            for c in range(n):
+                if r == 0 or r==m-1 or c==0 or c==n-1 and board[r][c] == "O":
                     dfs(r, c)
 
-        # 2) "O" -> "X": O(n*m)
-        for r in range(ROWS):
-            for c in range(COLS):
+        # 2. surrounded portion: O -> X
+        for r in range(m):
+            for c in range(n):
                 if board[r][c] == "O":
                     board[r][c] = "X"
 
-        # 3) "V" -> "O"
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "V":
+        # 3. reverse: T -> O
+        for r in range(m):
+            for c in range(n):
+                if board[r][c] == "T":
                     board[r][c] = "O"
-        
