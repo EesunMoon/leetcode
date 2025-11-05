a5fbfa2eac652cf7145b8a96140f6462d7a031de
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-05 18:55:06 -0500

    Time: 23 ms (80.4%), Space: 18.4 MB (40.95%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.py
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.py
@@ -1,39 +1,26 @@
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
         """
-        calculate the frequency both in tops and bottoms
-        tops: 2:set(0,3,5,6), 1:1, 4:1
-        bottoms: 5:1, 2:3, 6:1,3:1
-        total Sum: 
-        heap []
-        1. maximum total freq of sum < 6 --> invalid
-        2. 
+        let's assume we want to make X
+        (tops[i] == X or bottom[i] == X) or (tops[i]==X and bottoms[i] == X)
+        candidates should be 2 or 1
+            candidatates = tops[0], bottoms[0]
+        check -> min(tops[i] != candidates, bottoms[i] != candidates)
+        min(check(tops[0]), check(bottoms[0]))
         """
-        topMap = defaultdict(set) # num: idx set O(N)
-        bottomMap = defaultdict(set)
-        totalFreq = {} # num: freq O(6)
         n = len(tops)
-        for i in range(n):
-            topMap[tops[i]].add(i)
-            bottomMap[bottoms[i]].add(i)
-            totalFreq[tops[i]] = 1 + totalFreq.get(tops[i], 0)
-            totalFreq[bottoms[i]] = 1 + totalFreq.get(bottoms[i], 0)
+        def check(candidate):
+            numTops = 0
+            numBottoms = 0
+            for i in range(n):
+                if tops[i] != candidate and bottoms[i] != candidate:
+                    return float("INF") # invalid
+                if tops[i] != candidate:
+                    numTops += 1
+                if bottoms[i] != candidate:
+                    numBottoms += 1
+            return min(numTops, numBottoms)
         
-        maxHeap = [] # O(6log6) maxHeap
-        for num, freq in totalFreq.items():
-            heapq.heappush(maxHeap, (-freq, num))
-        
-        if -maxHeap[0][0] < n:
-            return -1
-
-        while maxHeap:
-            freq, num = heapq.heappop(maxHeap)
-            if -freq < n:
-                return -1
-            topSet = topMap[num]
-            bottomSet = bottomMap[num]
-            common = topSet.intersection(bottomSet)
-            if len(topSet) + len(bottomSet) - len(common) >= n:
-                return min(len(bottomSet) - len(common), len(topSet)-len(common))
-            
-        return -1
+        cand1, cand2 = tops[0], bottoms[0]
+        ans = min(check(cand1), check(cand2))
+        return ans if ans != float("INF") else -1
