bcfc743e0f90a677b3531643dc5934c11dbfafd9
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-05 03:17:05 -0500

    Time: 6 ms (73.03%), Space: 19.4 MB (26.64%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -1,5 +1,25 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj = defaultdict(list)
+        for u, v, w in flights:
+            adj[u].append([v,w])
+        
+        prices = [float("INF")] * n
+        prices[src] = 0
+        q = deque([(src, 0)]) # source and cost
+
+        while q and k >= 0:
+            k -= 1
+            for _ in range(len(q)):
+                node, cost = q.popleft()
+                for neiNode, neiCost in adj[node]:
+                    newCost = cost + neiCost
+                    if newCost < prices[neiNode]:
+                        prices[neiNode] = newCost
+                        q.append((neiNode, prices[neiNode]))
+        return prices[dst] if prices[dst]!=float("inf") else -1
+
+        """
         prices = [float("INF")] * n
         prices[src] = 0
 
@@ -14,4 +34,4 @@ class Solution:
                     tmpPrices[d] = prices[s] + cost
             prices = tmpPrices
         return prices[dst] if prices[dst] != float("INF") else -1
-
+        """
