227dfe514efb6865f36045f2b41495080a365a92
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-05 16:42:23 -0500

    Time: 0 ms (100%), Space: 17.7 MB (60.43%) - LeetHub

---
+++ 0961-long-pressed-name/0961-long-pressed-name.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        """
+        aaaalex, aaaaalleex
+            l         lr
+        aaaa, l, e, x
+        aaaaa, ll, ee, x
+        saeed, ssaaedd
+        1. use the sliding window
+        2. compare each sliding window
+            - length: nameS <= typedS
+            - char: same
+        """
+
+        lName, rName = 0, 0
+        lTyped, rTyped = 0, 0
+        while rName < len(name) and rTyped < len(typed):
+            if name[lName] != typed[lTyped]:
+                return False
+
+            while (rName+1) < len(name) and name[rName] == name[rName+1]:
+                rName += 1
+
+            while (rTyped+1) < len(typed) and typed[rTyped] == typed[rTyped+1]:
+                rTyped += 1
+            
+            if (rName-lName+1) > (rTyped-lTyped+1):
+                return False
+            
+            rName += 1
+            rTyped += 1
+            lName, lTyped = rName, rTyped
+        
+        if rTyped == len(typed) and rName == len(name):
+            return True
+        return False
