7ecfe2747040ebe6f1a39d99ef99092ff04d7fff
Eesun <55704325+EesunMoon@users.noreply.github.com>
2025-11-05 20:50:12 -0500

    Time: 231 ms (27.02%), Space: 48.6 MB (36.94%) - LeetHub

--- 1249-snapshot-array/1249-snapshot-array.py
+++ 1249-snapshot-array/1249-snapshot-array.py
@@ -1,36 +1,47 @@
 class SnapshotArray:
 
     def __init__(self, length: int):
-        self.shot = [[[0,0]] for _ in range(length)] # [id, val]
-        self.id = 0
+        """
+        row: index, store [snapID, val]
+        # snapArray: array-like
+        # snapTime: int - the total num of times we called snap
+        """
+        self.snapshot = [[[0,0]] for _ in range(length)]
+        self.snapTime = 0
+        
 
     def set(self, index: int, val: int) -> None:
-        if self.shot[index][-1][0] != self.id:
-            self.shot[index].append([self.id, val])
+        # snapArray[index] = val
+        """
+        if curr row list [-1] snapID modify
+        else -> add
+        """
+        if self.snapshot[index][-1][0] == self.snapTime:
+            self.snapshot[index][-1][1] = val
         else:
-            self.shot[index][-1][1] = val
+            self.snapshot[index].append([self.snapTime, val])
 
     def snap(self) -> int:
-        self.id += 1
-        return self.id -1
-        
-    def get(self, index: int, snap_id: int) -> int:
-        target = self.shot[index]
-        l, r = 0, len(target)-1
+        # snapTime - 1
+        self.snapTime += 1
+        return self.snapTime - 1
 
-        resId = 0
+    def get(self, index: int, snap_id: int) -> int:
+        # index: [snapId, val]
+        # binary searh: stored_snap_id <= snap_id
+        target = self.snapshot[index]
+        l, r = 0, len(target)-1
+        res = 0
         while l<=r:
-            m = (l+r)//2
+            m = (l+r) // 2
             if target[m][0] == snap_id:
                 return target[m][1]
             elif target[m][0] > snap_id:
                 r = m - 1
             else:
-                resId = max(resId, m)
+                res = target[m][1]
                 l = m + 1
-        return target[resId][1]
-
-        
+        return res
 
 
 # Your SnapshotArray object will be instantiated and called as such:
